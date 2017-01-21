843dfddcc8c6df95c5f7468298948e2eef385ad3
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-01-21 13:20:35 -0500

    Create Introduction to Nim Game.cpp

---
+++ Introduction to Nim Game/Introduction to Nim Game.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int g;
+    for(cin >> g; g > 0; g--) {
+        int n, result = 0;
+        cin >> n;
+        for(auto i = 0; i != n; i++) {
+            int j;
+            cin >> j;
+            result = result ^ j;
+        }
+        if(result == 0) {
+            cout << "Second\n";
+        } else {
+            cout << "First\n";
+        }
+    }
+    return 0;
+}
