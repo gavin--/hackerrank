8c8d2f441d75f05607f87416a589710e0f3178be
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-01-21 12:52:16 -0500

    Create Coinage.cpp

---
+++ Coinage/Coinage.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    for(cin >> T; T > 0; T--) {
+        auto result = 0ll;
+        int N, A, B, C, D;
+        cin >> N >> A >> B >> C >> D;
+        for(auto b = 0; b <= min(B, N / 2); b++) {
+            auto rb = N - 2 * b;
+            for(auto c = 0; c <= C; c++) {
+                auto rc = rb - 5 * c;
+                if(rc < 0) {
+                    break;
+                }
+                for(auto d = 0; d <= D; d++) {
+                    auto a = rc - 10 * d;
+                    if(a < 0) {
+                        break;
+                    }
+                    if(a > A) {
+                        continue;
+                    }
+                    result++;
+                }
+            }
+        }
+        cout << result << endl;
+    }
+    return 0;
+}
