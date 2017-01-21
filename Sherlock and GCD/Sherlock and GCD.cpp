24b64177714972b52a30ab74da90cf9fffd34593
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-01-21 00:48:48 -0500

    Create Sherlock and GCD.cpp

---
+++ Sherlock and GCD/Sherlock and GCD.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    while(b != 0) {
+        auto t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+int main() {
+    int T;
+    for(cin >> T; T > 0; T--) {
+        int N;
+        cin >> N;
+        int A[N];
+        for(auto i = 0; i != N; i++) {
+            cin >> A[i];
+        }
+        auto d = A[0];
+        for(auto i = 1; i < N; i++) {
+            d = gcd(A[i], d);
+        }
+        if(d > 1) {
+            cout << "NO" << endl;
+        } else {
+            cout << "YES" << endl;
+        }
+    }
+    return 0;
+}
