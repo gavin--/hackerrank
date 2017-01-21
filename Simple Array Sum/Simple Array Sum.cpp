e6718505e5b89cefe9c4a1f8bb4f36c2c556b5b1
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-01-21 00:15:44 -0500

    Create Simple Array Sum.cpp

---
+++ Simple Array Sum/Simple Array Sum.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, sum = 0;
+    cin >> n;
+    for(int i; cin >> i;){
+       sum += i;
+    }
+    cout << sum;
+    return 0;
+}
