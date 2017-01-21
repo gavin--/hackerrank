7f28ca993e6f0d83f91fe9b8ee04100bc0172cf3
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-01-21 12:53:43 -0500

    Create Building a List.cpp

---
+++ Building a List/Building a List.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<string> generate(string S) {
+    vector<string> result;
+    for(auto i = 0; i != S.size(); i++) {
+        auto current = generate(S.substr(i + 1));
+        for(auto& j : current) {
+            j.insert(0, 1, S[i]);
+        }
+        result.emplace_back(string(1, S[i]));
+        result.insert(end(result), begin(current), end(current));
+    }
+    return result;
+} 
+
+void print(string& S) {
+    for(auto& j : generate(S)) {
+        cout << j << endl;
+    }
+}
+
+int main() {
+    int T;
+    for(cin >> T; T > 0; T--) {
+        int N;
+        string S;
+        cin >> N >> S;
+        sort(begin(S), end(S));
+        print(S);
+    }
+    return 0;
+}
