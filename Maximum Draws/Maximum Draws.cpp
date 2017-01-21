76ed076e6a8ff495c480634848ebb873b451604a
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-01-21 12:32:43 -0500

    Create Maximum Draws.cpp

---
+++ Maximum Draws/Maximum Draws.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    for(cin >> T; T > 0; T--) {
+        int N;
+        cin >> N;
+        cout << N + 1 << endl;
+    }
+    return 0;
+}
