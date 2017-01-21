e3711cbba18ce20da55665327ac5f14abb16eab6
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-01-21 00:09:19 -0500

    Update Solve Me First.cpp

--- Solve Me First.cpp
+++ Solve Me First.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int solveMeFirst(int a, int b) {
-        return a + b;
+    return a + b;
 }
 
 int main() {
-      int num1, num2;
-      cin >> num1 >> num2;
-      auto sum = solveMeFirst(num1,num2);
-      cout << sum;
-      return 0;
+    int num1, num2;
+    cin >> num1 >> num2;
+    auto sum = solveMeFirst(num1,num2);
+    cout << sum;
+    return 0;
 }
