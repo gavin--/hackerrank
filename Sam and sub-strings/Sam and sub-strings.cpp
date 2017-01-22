8813e573679e5af6aa9eb8cc14df48652eaccb7f
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-01-22 14:57:58 -0500

    Create Sam and sub-strings.cpp

---
+++ Sam and sub-strings/Sam and sub-strings.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+const int MOD = 1000000007, INVERSE = 111111112;
+
+long long pow(long long n, int x) {
+    auto result = 1;
+    while(x > 0) {
+        if(x % 2 == 1) {
+            result = (result * n) % MOD;
+        }
+        n = (n * n) % MOD;
+        x /= 2;
+    }
+    return result;
+}
+
+int main() {
+    string N;
+    cin >> N;
+    auto result = 0;
+    for(auto i = 0; i != N.size(); i++) {
+        auto count = (i + 1) * (N[i] - '0');
+        result = (result + count * (pow(10, N.size() - i) - 1) % MOD * INVERSE) % MOD;
+    }
+    cout << result;
+    return 0;
+}
