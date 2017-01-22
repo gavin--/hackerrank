e7b3b548626765f365313b1f3f756b90686d6470
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-01-22 13:14:20 -0500

    Create Maximum Subarray Sum.cpp

---
+++ Maximum Subarray Sum/Maximum Subarray Sum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int q;
+    for(cin >> q; q > 0; q--) {
+        long long n, m;
+        cin >> n >> m;
+        pair<long long, int> sum[n];
+        for(auto i = 0ll, j = 0ll; i != n; i++) {
+            int k;
+            cin >> k;
+            j = (j + k) % m;
+            sum[i] = {j, i};
+        }
+        sort(sum, sum + n);
+        auto result = sum[n - 1].first;
+        for(auto i = 1; i < n; i++) {
+            if(sum[i - 1].second > sum[i].second) {
+                result = max(result, m - sum[i].first + sum[i - 1].first);
+            }
+        }
+        cout << result << endl;
+    }
+    return 0;
+}
