5118fba35d32479ec8b934c80e80e223bd8ad7ae
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-01-25 16:55:24 -0500

    Create Little Ashish's Huge Donation.cpp

---
+++ Little Ashish-s Huge Donation/Little Ashish's Huge Donation.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+using namespace std;
+
+long long f(long long n) {
+    return n * (n + 1) * (2 * n + 1) / 6;
+}
+
+int main() {
+    int T;
+    for(cin >> T; T > 0; T--) {
+        long long X;
+        cin >> X;
+        auto begin = 1ll, end = begin;
+        while(f(end) < X) {
+            end *= 2;
+        }
+        while(begin < end) {
+            auto mid = (begin + end + 1) / 2;
+            if(f(mid) > X) {
+                end = mid - 1;
+            } else {
+                begin = mid;
+            }
+        }
+        cout << begin << endl;
+    }
+    return 0;
+}
