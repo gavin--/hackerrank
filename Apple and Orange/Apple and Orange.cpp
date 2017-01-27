05d3f4c74c2468bab2c9da1cc7be90219ff3483b
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-01-26 21:40:42 -0500

    Create Apple and Orange.cpp

---
+++ Apple and Orange/Apple and Orange.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int s, t, a, b, m, n, apple = 0, orange = 0;
+    cin >> s >> t >> a >> b >> m >> n;
+    for(auto i = 0; i != m; i++) {
+        int j;
+        cin >> j;
+        if(a + j >=s && a + j <= t) {
+            apple++;
+        }
+    }
+    for(auto i = 0; i != n; i++) {
+        int j;
+        cin >> j;
+        if(b + j >=s && b + j <= t) {
+            orange++;
+        }
+    }
+    cout << apple << endl << orange;
+    return 0;
+}
