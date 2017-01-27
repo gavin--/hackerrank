348815818549673fa38b7b6a9976f96b862da1d4
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-01-26 21:39:45 -0500

    Create Kangaroo.cpp

---
+++ Kangaroo/Kangaroo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x1, v1, x2, v2;
+    cin >> x1 >> v1 >> x2 >> v2;
+    if(v1 == v2) {
+        if(x1 == x2) {
+            cout << "YES";
+        } else {
+            cout << "NO";
+        }
+        return 0;
+    }
+    if((x1 - x2) % (v2 - v1) == 0 && (x1 - x2) / (v2 - v1) >= 0) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+    return 0;
+}
