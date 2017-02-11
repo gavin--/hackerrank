078e189e983ede416b2aa9ccdd2674df3a3f6699
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-02-11 18:54:53 -0500

    Update Beautiful Quadruples.cpp

--- Beautiful Quadruples/Beautiful Quadruples.cpp
+++ Beautiful Quadruples/Beautiful Quadruples.cpp
@@ -4,27 +4,27 @@
 using namespace std;
 
 int main() {
-	int input[4];
-	cin >> input[0] >> input[1] >> input[2] >> input[3];
-	sort(begin(input), end(input));
-	map<int, int> count;
-	auto total = 0;
-	for(auto i = 1; i <= input[2]; i++) {
-		for(auto j = i; j <= input[3]; j++) {
-			count[i ^ j]++;
-			total++;
-		}
-	}
+    int input[4];
+    cin >> input[0] >> input[1] >> input[2] >> input[3];
+    sort(begin(input), end(input));
+    map<int, int> count;
+    auto total = 0;
+    for(auto i = 1; i <= input[2]; i++) {
+        for(auto j = i; j <= input[3]; j++) {
+            count[i ^ j]++;
+            total++;
+        }
+    }
     auto result = 0ll;
-	for(auto i = 1; i <= input[1]; i++) {
-		for(auto j = 1; j <= min(input[0], i); j++) {
-			result += total - count[j ^ i];
-		}
-		for(auto j = i; j <= input[3]; j++) {
-			count[i ^ j]--;
-			total--;
-		}
-	}
-	cout << result;
+    for(auto i = 1; i <= input[1]; i++) {
+        for(auto j = 1; j <= min(input[0], i); j++) {
+            result += total - count[j ^ i];
+        }
+        for(auto j = i; j <= input[3]; j++) {
+            count[i ^ j]--;
+            total--;
+        }
+    }
+    cout << result;
     return 0;
 }
