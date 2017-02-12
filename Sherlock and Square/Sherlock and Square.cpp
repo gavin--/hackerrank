211afd1ffd4393167b387694809b76c98057d32e
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-02-11 23:29:09 -0500

    Create Sherlock and Square.cpp

---
+++ Sherlock and Square/Sherlock and Square.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+const long long MOD = 1000000007;
+
+long long pow(long long x, long long n) {
+    auto result = 1ll;
+    while(n != 0) {
+        if(n % 2 == 1) {
+            result = result * x % MOD;
+        }
+        n /= 2;
+        x = x * x % MOD;
+    }
+    return result;
+}
+
+int main() {
+    int T;
+    for(cin >> T; T > 0; T--) {
+        long long N;
+        cin >> N;
+        cout << (pow(2, N + 1) + 2) % MOD << endl;
+    }
+    return 0;
+}
