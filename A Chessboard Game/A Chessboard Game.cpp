4d4f6429fc756e46fb707a3012768b25d496f33c
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-02-18 16:56:41 -0500

    Create A Chessboard Game.cpp

---
+++ A Chessboard Game/A Chessboard Game.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    for(cin >> T; T > 0; T--) {
+        int x, y;
+        cin >> x >> y;
+        auto r1 = (x - 1) % 4, r2 = (y - 1) % 4;
+        if((r1 == 0 || r1 == 1) && (r2 == 0 || r2 == 1)) {
+            cout << "Second";
+        } else {
+            cout << "First";
+        }
+        cout << endl;
+    }
+    return 0;
+}
