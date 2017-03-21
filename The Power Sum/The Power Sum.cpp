d6394e49de1636ebd32b155ffdbdf7726c0ced6a
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-03-21 16:15:42 -0400

    Update The Power Sum.cpp

--- The Power Sum/The Power Sum.cpp
+++ The Power Sum/The Power Sum.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int pow(int X, int N) {
-    auto result = 1;
+    auto result = 1ll;
     while(N != 0) {
         if(N % 2 == 1) {
             result *= X;
@@ -20,15 +20,19 @@ int main() {
     int dp[X + 1][X + 1];
     fill(dp[0], dp[0] + X + 1, 1);
     for(auto i = 1; i <= X; i++) {
-        for(auto j = 0; j <= X; j++) {
-            dp[i][j] = 0;
-            for(auto k = 0; k < j; k++) {
-                auto l = pow(k, N);
-                if(l > i) {
-                    break;
-                }
-                dp[i][j] += dp[i - l][k];
+        dp[i][0] = 0;
+        auto j = 1;
+        while(true) {
+            dp[i][j] = dp[i][j - 1];
+            auto k = pow(j, N);
+            if(k > i) {
+                break;
             }
+            dp[i][j] += dp[i - k][j - 1];
+            j++;
+        }
+        for(; j <= X; j++) {
+            dp[i][j] = dp[i][j - 1];
         }
     }
     cout << dp[X][X];
