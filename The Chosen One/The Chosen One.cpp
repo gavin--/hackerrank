236432d238620950ea1838d13cddf13b8b42c6d8
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-03-30 20:48:21 -0400

    Create The Chosen One.cpp

---
+++ The Chosen One/The Chosen One.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+long long gcd(long long a, long long b) {
+    while(b != 0) {
+        auto t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    long long a[n];
+    for(auto i = 0; cin >> a[i]; i++) {
+    }
+    if(n == 1) {
+        cout << a[0] + 1;
+    } else {
+        long long prefix[n + 1], postfix = 0;
+        prefix[0] = 0;
+        for(auto i = 1; i <= n; i++) {
+            prefix[i] = gcd(a[i - 1], prefix[i - 1]);
+        }
+        for(auto i = n; i > 0; i--) {
+            auto g = gcd(prefix[i - 1], postfix);
+            if(a[i - 1] % g != 0) {
+                cout << g;
+                break;
+            }
+            postfix = gcd(a[i - 1], postfix);
+        }
+    }
+    return 0;
+}
