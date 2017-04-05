69fcee45fb9388e95f342bbcf5229f61e6bc158a
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-05 13:00:34 -0400

    Create Euler's Criterion.cpp

---
+++ Euler-s Criterion/Euler's Criterion.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+long long pow(long long x, long long n, long long mod) {
+    auto result = 1ll;
+    while(n != 0) {
+        if(n % 2 == 1) {
+            result = result * x % mod;
+        }
+        n /= 2;
+        x = x * x % mod;
+    }
+    return result;
+}
+
+int main() {
+    int T;
+    for(cin >> T; T > 0; T--) {
+        long long A, M;
+        cin >> A >> M;
+        if(M == 2) {
+            cout << "YES\n";
+        } else {
+            if(pow(A, (M - 1) / 2, M) == 1 || A == 0) {
+                cout << "YES\n";
+            } else {
+                cout << "NO\n";
+            }
+        }
+    }
+    return 0;
+}
