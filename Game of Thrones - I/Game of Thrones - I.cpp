cea64270e8db897c8bc60d103acb964a029e884c
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-09 19:03:07 -0400

    Update Game of Thrones - I.cpp

--- Game of Thrones - I/Game of Thrones - I.cpp
+++ Game of Thrones - I/Game of Thrones - I.cpp
@@ -9,11 +9,11 @@ int main() {
     for(auto& i : s) {
         count[i]++;
     }
-    bool mid = false;
+    auto mid = s.size() % 2 == 1;
     for(auto& i : count) {
         if(i.second % 2 == 1) {
-            if(s.size() % 2 == 1 && mid == false) {
-                mid = true;
+            if(mid) {
+                mid = false;
             } else {
                 cout << "NO";
                 return 0;
