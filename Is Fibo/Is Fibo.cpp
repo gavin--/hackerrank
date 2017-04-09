07fdd60d033d62f1b2a090010263aa25ba96a562
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-09 19:45:05 -0400

    Update Is Fibo.cpp

--- Is Fibo/Is Fibo.cpp
+++ Is Fibo/Is Fibo.cpp
@@ -4,24 +4,19 @@ using namespace std;
 
 int main() {
     int T;
-    long long prev2 = 0, prev1 = 1;
     vector<long long> fib {0, 1};
-    while(prev2 + prev1 <= 10000000000) {
-        auto t = prev2 + prev1;
-        fib.emplace_back(t);
-        prev2 = prev1;
-        prev1 = t;
+    while(fib.back() + fib.rbegin()[1] <= 10000000000) {
+        fib.emplace_back(fib.back() + fib.rbegin()[1]);
     } 
     for(cin >> T; T > 0; T--) {
         long long N;
         cin >> N;
         auto it = lower_bound(begin(fib), end(fib), N);
         if(it != end(fib) && *it == N) {
-            cout << "IsFibo";
+            cout << "IsFibo\n";
         } else {
-            cout << "IsNotFibo";
+            cout << "IsNotFibo\n";
         }
-        cout << endl;
     }
     return 0;
 }
