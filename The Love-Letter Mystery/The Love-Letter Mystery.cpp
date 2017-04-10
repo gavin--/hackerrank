07aa226b6181ed824221efb2731090b2af3270c2
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-09 21:02:26 -0400

    Update The Love-Letter Mystery.cpp

--- The Love-Letter Mystery/The Love-Letter Mystery.cpp
+++ The Love-Letter Mystery/The Love-Letter Mystery.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        string s;
-        cin >> s;
+    cin >> T;
+    for(string s; cin >> s; ) {
         auto result = 0;
         for(auto i = 0; i < s.size() / 2; i++) {
-            result += abs(s[i] - s[s.size() - 1 - i]);
+            result += abs(s[i] - s.rbegin()[i]);
         }
         cout << result << endl;
     }
