64dfe61b3894161656db81f7230293d689269f50
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-09 21:07:45 -0400

    Update Sherlock and Array.cpp

--- Sherlock and Array/Sherlock and Array.cpp
+++ Sherlock and Array/Sherlock and Array.cpp
@@ -3,24 +3,23 @@ using namespace std;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        int N, sum = 0;
-        cin >> N;
+    cin >> T;
+    for(int N; cin >> N; ) {
+        auto sum = 0;
         int A[N];
         for(auto i = 0; i != N; i++) {
             cin >> A[i];
             sum += A[i];
         }
+        auto result = "NO\n";
         for(auto i = 0, j = 0; i != N; i++) {
             if(j == sum - j - A[i]) {
-                cout << "YES\n";
-                goto next;
+                result = "YES\n";
+                break;
             }
             j += A[i];
         }
-        cout << "NO\n";
-        next:
-        ;
+        cout << result;
     }
     return 0;
 }
