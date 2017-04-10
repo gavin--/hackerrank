e800c5ec8a4b4af90d867f26c73caed4c54cb584
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-09 20:59:52 -0400

    Update Palindrome Index.cpp

--- Palindrome Index/Palindrome Index.cpp
+++ Palindrome Index/Palindrome Index.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 bool palindrome(string S) {
     for(auto i = 0; i < S.size() / 2; i++) {
-        if(S[i] != S[S.size() - i - 1]) {
+        if(S[i] != S.rbegin()[i]) {
             return false;
         }
     }
@@ -12,23 +12,20 @@ bool palindrome(string S) {
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        string S;
-        cin >> S;
+    cin >> T;
+    for(string S; cin >> S; ) {
+        auto result = -1;
         for(auto i = 0; i < S.size() / 2; i++) {
-            if(S[i] != S[S.size() - i - 1]) {
+            if(S[i] != S.rbegin()[i]) {
                 if(palindrome(S.substr(0, i) + S.substr(i + 1, S.size()))) {
-                    cout << i << endl;
-                    goto next;
+                    result = i;
                 } else {
-                    cout << S.size() - i - 1 << endl;
+                    result = S.size() - i - 1;
                 }
-                goto next;
+                break;
             }
         }
-        cout << -1 << endl;
-        next:
-        ;
+        cout << result << endl;
     }
     return 0;
 }
