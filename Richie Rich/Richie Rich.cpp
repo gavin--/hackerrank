b481bbe1bc7c749c570395e95c603f9d6350aa27
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-09 20:54:39 -0400

    Update Richie Rich.cpp

--- Richie Rich/Richie Rich.cpp
+++ Richie Rich/Richie Rich.cpp
@@ -6,35 +6,33 @@ int main() {
     string s;
     cin >> n >> k >> s;
     for(auto i = 0; i < s.size() / 2; i++) {
-        if(s[i] != s[s.size() - i - 1]) {
+        if(s[i] != s.rbegin()[i]) {
             k--;
         }
     }
     if(k < 0) {
-        cout << -1;
-        return 0;
-    }
-    for(auto i = 0; i < s.size() / 2; i++) {
-        if(s[i] != s[s.size() - i - 1]) {
-            if(k > 0 && max(s[i], s[s.size() - i - 1]) != '9') {
-                s[i] = '9';
-                s[s.size() - i - 1] = '9';
-                k--;
-            } else {
-                if(s[i] > s[s.size() - i - 1]) {
-                    s[s.size() - i - 1] = s[i];
+        s = "-1";
+    } else {
+        for(auto i = 0; i < s.size() / 2; i++) {
+            if(s[i] != s.rbegin()[i]) {
+                if(k > 0 && max(s[i], s.rbegin()[i]) != '9') {
+                    s.rbegin()[i] = s[i] = '9';
+                    k--;
                 } else {
-                    s[i] = s[s.size() - i - 1];
+                    if(s[i] > s.rbegin()[i]) {
+                        s.rbegin()[i] = s[i];
+                    } else {
+                        s[i] = s.rbegin()[i];
+                    }
                 }
+            } else if(s[i] != '9' && k >= 2) {
+                s.rbegin()[i] = s[i] = '9';
+                k -= 2;
             }
-        } else if(s[i] != '9' && k >= 2) {
-            s[i] = '9';
-            s[s.size() - i - 1] = '9';
-            k -= 2;
         }
-    }
-    if(k > 0 && s.size() % 2 == 1) {
-        s[s.size() / 2] = '9';
+        if(k > 0 && s.size() % 2 == 1) {
+            s[s.size() / 2] = '9';
+        }
     }
     cout << s;
     return 0;
