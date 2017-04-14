5d9806d04c746e41b77c837c5369b6738fb52f11
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-13 20:34:00 -0400

    Update Stone Division, Revisited.cpp

--- Stone Division- Revisited/Stone Division, Revisited.cpp
+++ Stone Division- Revisited/Stone Division, Revisited.cpp
@@ -6,15 +6,13 @@ int main() {
     int q;
     cin >> q;
     for(long long n, m; cin >> n >> m; ) {
-        long long S[m + 1];
+        long long S[m + 1], dp[m + 1], result = 0;
         for(auto i = 1; i <= m; i++) {
             cin >> S[i];
         }
         S[0] = n;
         sort(S + 1, S + m + 1, greater<long long>());
-        long long dp[m + 1], result = 0;
-        dp[0] = 0;
-        for(auto i = 1; i <= m; i++) {
+        for(auto i = 0; i <= m; i++) {
             dp[i] = 0;
             for(auto j = 0; j < i; j++) {
                 if(S[j] % S[i] == 0 && S[i] != S[j] && dp[j] + n / S[j] > dp[i]) {
