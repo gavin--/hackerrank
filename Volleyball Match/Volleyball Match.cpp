55b7893a23192f3ee4e0dce3d61dd32c1c69034f
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-13 22:33:29 -0400

    Update Volleyball Match.cpp

--- Volleyball Match/Volleyball Match.cpp
+++ Volleyball Match/Volleyball Match.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 const long long MOD = 1000000007;
 
-long long exp(long long x, int n) {
+long long pow(long long x, int n) {
     auto result = 1;
     while(n != 0) {
         if(n % 2 == 1) {
@@ -16,16 +16,16 @@ long long exp(long long x, int n) {
 }
 
 long long inverse(long long n) {
-    return exp(n, MOD - 2);
+    return pow(n, MOD - 2);
 }
 
 long long C(long long n, long long k) {
-    auto result = 1ll, temp = 1ll;
+    auto top = 1ll, bottom = 1ll;
     for(auto i = 1; i <= k; i++) {
-        result = result * (n - i + 1) % MOD;
-        temp = temp * i % MOD;
+        top = top * (n - i + 1) % MOD;
+        bottom = bottom * i % MOD;
     }
-    return result * inverse(temp) % MOD;
+    return top * inverse(bottom) % MOD;
 }
 
 int main() {
@@ -33,12 +33,12 @@ int main() {
     cin >> A >> B;
     if(A < 24 && B != 25 || A != 25 && B < 24 || A >= 24 && B >= 24 && abs(A - B) != 2) {
         cout << 0;
-    } else if(A < 25 || B < 25) {
+    } else if(A <= 25 || B <= 25) {
         A = min(A, 24);
         B = min(B, 24);
         cout << C(A + B, B);
     } else {
-        cout << (C(48, 24) * exp(2, min(A, B) - 24)) % MOD;
+        cout << C(48, 24) * pow(2, min(A, B) - 24) % MOD;
     }
     return 0;
 }
