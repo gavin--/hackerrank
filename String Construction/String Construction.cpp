eca16a71bcc1f7d22fee1dc703c5d5bcaf7c6778
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-22 00:19:25 -0400

    Update String Construction.cpp

--- String Construction/String Construction.cpp
+++ String Construction/String Construction.cpp
@@ -4,14 +4,9 @@ using namespace std;
 
 int main() {
     int n;
-    for(cin >> n; n > 0; n--) {
-        string s;
-        cin >> s;
-        set<char> set;
-        for(auto& i : s) {
-            set.emplace(i);
-        }
-        cout << set.size() << endl;
+    cin >> n;
+    for(string s; cin >> s; ) {
+        cout << set<char>(s.begin(), s.end()).size() << endl;
     }
     return 0;
 }
