d4bd5eef28d8e82401de1790ae4b9187d1fe21d3
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-24 17:08:20 -0400

    Create AND Product.cpp

---
+++ AND Product/AND Product.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(long long a, b; cin >> a >> b; ) {
+        auto c = LLONG_MAX;
+        b ^= a;
+        while(b >>= 1) {
+            c <<= 1;
+        }
+        cout << (a & c) << endl;
+    }
+    return 0;
+}
