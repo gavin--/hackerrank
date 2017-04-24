ce81b9626a0f7fe9bb270ab9fffda45ae6af082f
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-23 20:14:33 -0400

    Update Sequence Equation.cpp

--- Sequence Equation/Sequence Equation.cpp
+++ Sequence Equation/Sequence Equation.cpp
@@ -4,17 +4,15 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int p[n];
+    int p[n], result[n];
     for(auto i = 0; cin >> p[i]; i++) {
         p[i]--;
     }
     for(auto i = 0; i != n; i++) {
-        for(auto j = 0; j != n; j++) {
-            if(p[p[j]] == i) {
-                cout << j + 1 << endl;
-                break;
-            }
-        }
+        result[p[p[i]]] = i + 1;
+    }
+    for(auto i = 0; i != n; i++) {
+        cout << result[i] << endl;
     }
     return 0;
 }
