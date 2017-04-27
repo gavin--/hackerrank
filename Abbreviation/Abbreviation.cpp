57e1d8f056a546fe56e54d0c384a2a6d03163ec6
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-27 18:28:05 -0400

    Update Abbreviation.cpp

--- Abbreviation/Abbreviation.cpp
+++ Abbreviation/Abbreviation.cpp
@@ -1,34 +1,32 @@
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int main() {
     int q;
-    for(cin >> q; q > 0; q--) {
-        string a, b;
-        cin >> a >> b;
-        vector<bool> dp[a.size() + 1];
-        fill(dp, dp + a.size() + 1, vector<bool>(b.size() + 1));
+    cin >> q;
+    for(string a, b; cin >> a >> b; ) {
+        bool dp[2][a.size() + 1], cur = 0, pre = 1;
         dp[0][0] = 1;
         for(auto i = 1; i <= a.size(); i++) {
-            dp[i][0] = islower(a[i - 1]);
+            dp[0][i] = islower(a[i - 1]);
         }
         for(auto i = 1; i <= b.size(); i++) {
+            swap(cur, pre);
+            dp[cur][i - 1] = 0;
             for(auto j = i; j <= a.size(); j++) {
                 if(islower(a[j - 1])) {
-                    dp[j][i] = dp[j - 1][i];
+                    dp[cur][j] = dp[cur][j - 1];
                     if(toupper(a[j - 1]) == b[i - 1] || a[j - 1] == b[i - 1]) {
-                        dp[j][i] = dp[j][i] || dp[j - 1][i - 1];
+                        dp[cur][j] = dp[cur][j] || dp[pre][j - 1];
                     }
                 } else if(b[i - 1] == a[j - 1]) {
-                    dp[j][i] = dp[j - 1][i - 1];
+                    dp[cur][j] = dp[pre][j - 1];
                 } else {
-                    dp[j][i] = 0;
+                    dp[cur][j] = 0;
                 }
             }
         }
-        if(dp[a.size()][b.size()]) {
+        if(dp[cur][a.size()]) {
             cout << "YES\n";
         } else {
             cout << "NO\n";
