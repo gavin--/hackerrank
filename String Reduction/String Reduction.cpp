432c67f87866fa09caaf0b0d071d0d22f831b55a
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-27 18:59:21 -0400

    Update String Reduction.cpp

--- String Reduction/String Reduction.cpp
+++ String Reduction/String Reduction.cpp
@@ -3,23 +3,19 @@ using namespace std;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        string s;
-        cin >> s;
+    cin >> T;
+    for(string s; cin >> s; cout << endl) {
         int count[3] = {};
         for(auto& i : s) {
             count[i - 'a']++;
         }
         if(count[0] == s.size() || count[1] == s.size() || count[2] == s.size()) {
-            cout << s.size() << endl;
-            continue;
-        }
-        if(count[0] % 2 == count[1] % 2 && count[1] % 2 == count[2] % 2) {
+            cout << s.size();
+        } else if(count[0] % 2 == count[1] % 2 && count[1] % 2 == count[2] % 2) {
             cout << 2;
         } else {
             cout << 1;
         }
-        cout << endl;
     }
     return 0;
 }
