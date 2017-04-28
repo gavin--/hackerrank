f438d768347d3552dd00af3f5b5e1ffacb5f0d94
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-04-27 23:57:10 -0400

    Update Choose and Calculate.cpp

--- Choose and Calculate/Choose and Calculate.cpp
+++ Choose and Calculate/Choose and Calculate.cpp
@@ -4,44 +4,40 @@
 using namespace std;
 
 const long long MOD = 1000000007;
+long long *factorial, *ifactorial, *inv;
 
-long long* cal(long long N, long long K) {
-    long long factorial[N], ifactorial[N], inv[N], *dp = new long long[N];
+void precompute(long long n, long long k) {
+    factorial = new long long[n], ifactorial = new long long[n], inv = new long long[n];
     factorial[0] = 1;
     ifactorial[0] = 1;
     inv[1] = 1;
     factorial[1] = 1;
     ifactorial[1] = 1;
-    dp[K - 2] = 1;
-    for(auto i = 2; i < N; i++) {
+    for(auto i = 2; i < n; i++) {
         factorial[i] = factorial[i - 1] * i % MOD;
         inv[i] = (-MOD / i + MOD) * inv[MOD % i] % MOD;
         ifactorial[i] = ifactorial[i - 1] * inv[i] % MOD;
     }
-    for(auto i = K - 1; i < N; i++) {
-        dp[i] = (dp[i - 1] + factorial[i] * ifactorial[K - 2] % MOD * ifactorial[i - K + 2]) % MOD;
-    }
-    return dp;
+}
+
+long long C(long long n, long long k) {
+    return factorial[n] * ifactorial[k] % MOD * ifactorial[n - k] % MOD;
 }
 
 int main() {
     long long N, K;
     cin >> N >> K;
-    if(K == 1) {
-        cout << 0;
-        return 0;
-    }
-    auto dp = cal(N, K);
+    precompute(N, K);
     int a[N];
     for(auto i = 0; cin >> a[i]; i++) {
     }
     sort(a, a + N);
     auto result = 0ll;
     for(auto i = K - 1; i < N; i++) {
-        result = (result + a[i] * dp[i - 1]) % MOD;
+        result = (result + a[i] * C(i, K - 1)) % MOD;
     }
     for(auto i = 0; i <= N - K; i++) {
-        result = (result - a[i] * dp[N - i - 2]) % MOD;
+        result = (result - a[i] * C(N - i - 1, K - 1)) % MOD;
     }
     cout << (result + MOD) % MOD;
     return 0;
