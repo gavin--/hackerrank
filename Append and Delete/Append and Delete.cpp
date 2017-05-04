83e6356882f9c4d7664556056bc16654f62cd7b9
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-03 22:31:09 -0400

    Create Append and Delete.cpp

---
+++ Append and Delete/Append and Delete.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s, t, result = "No";
+    int k;
+    cin >> s >> t >> k;
+    if(s == t && (k % 2 == 0 || k > 2 * s.size())) {
+        result = "Yes";
+    }
+    for(auto i = 0; i <= min(s.size(), t.size()); i++) {
+        if(s[i] != t[i]) {
+            int size = k - s.size() + i - t.size() + i;
+            if(size > 0 && (size % 2 == 0 || size > 2 * i)) {
+                result = "Yes";
+            }
+        }
+    }
+    cout << result;
+    return 0;
+}
