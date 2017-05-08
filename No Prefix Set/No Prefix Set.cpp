49983c8766143bace81b7080c830cd8182524da8
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-07 20:20:29 -0400

    Update No Prefix Set.cpp

--- No Prefix Set/No Prefix Set.cpp
+++ No Prefix Set/No Prefix Set.cpp
@@ -4,26 +4,25 @@ using namespace std;
 
 int main() {
     int N;
+    cin >> N;
     set<string> set1;
     set<string, greater<string>> set2;
-    for(cin >> N; N > 0; N--) {
-        string s;
-        cin >> s;
-        auto find1 = set1.lower_bound(s);
-        auto find2 = set2.lower_bound(s);
+    string result = "GOOD SET";
+    for(string s; cin >> s; ) {
+        auto find1 = set1.lower_bound(s), find2 = set2.lower_bound(s);
         if(find1 == set1.end() || (*find1).substr(0, s.size()) != s) {
             set1.emplace(s);
         } else {
-            cout << "BAD SET\n" << s;
-            return 0;
+            result = "BAD SET\n" + s;
+            break;
         }
         if(find2 == set2.end() || s.substr(0, (*find2).size()) != *find2) {
             set2.emplace(s);
         } else {
-            cout << "BAD SET\n" << s;
-            return 0;
+            result = "BAD SET\n" + s;
+            break;
         }
     }
-    cout << "GOOD SET";
+    cout << result;
     return 0;
 }
