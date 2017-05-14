806e650a624de19e7b85283d33caacfa4837f9d8
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-13 23:30:06 -0400

    Update Wet Shark and 42.cpp

--- Wet Shark and 42/Wet Shark and 42.cpp
+++ Wet Shark and 42/Wet Shark and 42.cpp
@@ -5,15 +5,13 @@ const long long MOD = 1000000007;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        long long S;
-        cin >> S;
+    cin >> T;
+    for(long long S; cin >> S; cout << endl) {
         if(S % 20 == 0) {
             cout << (S / 20 * 42 - 2) % MOD;
         } else {
             cout << (S / 20 * 42 + S % 20 * 2) % MOD;
         }
-        cout << endl;
     }
     return 0;
 }
