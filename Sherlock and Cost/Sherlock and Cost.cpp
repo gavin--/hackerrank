d13e193dbb4afda9bbcbfdb505dda927d31bd9fe
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-14 17:15:29 -0400

    Update Sherlock and Cost.cpp

--- Sherlock and Cost/Sherlock and Cost.cpp
+++ Sherlock and Cost/Sherlock and Cost.cpp
@@ -3,21 +3,17 @@ using namespace std;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        int N;
-        cin >> N;
+    cin >> T;
+    for(int N; cin >> N; ) {
         int B[N];
         for(auto i = 0; i != N; i++) {
             cin >> B[i];
         }
-        long long dp[N][2];
-        dp[0][0] = 0;
-        dp[0][1] = 0;
+        pair<long long, long long> dp = {0, 0};
         for(auto i = 1; i != N; i++) {
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + B[i - 1] - 1);
-            dp[i][1] = max(dp[i - 1][0] + B[i] - 1, dp[i - 1][1] + abs(B[i] - B[i - 1]));
+            dp = {max(dp.first, dp.second + B[i - 1] - 1), max(dp.first + B[i] - 1, dp.second + abs(B[i] - B[i - 1]))};
         }
-        cout << max(dp[N - 1][0], dp[N - 1][1]) << endl;
+        cout << max(dp.first, dp.second) << endl;
     }
     return 0;
 }
