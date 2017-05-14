d00d980b612865939788abc2783b5b5063e3f712
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-13 23:28:09 -0400

    Update Wet Shark and Two Subsequences.cpp

--- Wet Shark and Two Subsequences/Wet Shark and Two Subsequences.cpp
+++ Wet Shark and Two Subsequences/Wet Shark and Two Subsequences.cpp
@@ -9,40 +9,36 @@ int main() {
     int X[m], result = 0;
     for(auto i = 0; cin >> X[i]; i++) {
     }
-    int n = (r + s) / 2, l = (r - s) / 2, dp[2][n + 1][m + 1];
-    auto cur = 0, pre = 1;
+    int n = (r + s) / 2, l = (r - s) / 2, dp[n + 1][m + 1];
     for(auto j = 0; j <= n; j++) {
         for(auto k = 0; k <= m; k++) {
-            dp[cur][j][k] = 0;
+            dp[j][k] = 0;
         }
     }
-    dp[cur][0][0] = 1;
+    dp[0][0] = 1;
     if(X[0] <= n) {
-        dp[cur][X[0]][1] = 1;
+        dp[X[0]][1] = 1;
     }
     for(auto i = 1; i < m; i++) {
-        swap(cur, pre);
-        dp[cur][0][0] = 1;
+        dp[0][0] = 1;
         for(auto k = 1; k <= m; k++) {
-            dp[cur][0][k] = 0;
+            dp[0][k] = 0;
         }
-        for(auto j = 1; j <= n; j++) {
-            dp[cur][j][0] = 0;
+        for(auto j = n; j >= 1; j--) {
+            dp[j][0] = 0;
             for(auto k = 1; k <= m; k++) {
                 if(j < X[i]) {
-                    dp[cur][j][k] = dp[pre][j][k];
+                    dp[j][k] = dp[j][k];
                 } else {
-                    dp[cur][j][k] = (dp[pre][j - X[i]][k - 1] + dp[pre][j][k]) % MOD;
+                    dp[j][k] = (dp[j - X[i]][k - 1] + dp[j][k]) % MOD;
                 }
             }
         }
     }
-    if(l < 0 || (r + s) % 2 == 1 || (r - s) % 2 == 1 || r == 0) {
-        cout << 0;
-        return 0;
-    }
-    for(auto i = 0; i <= m; i++) {
-        result = (result + (long long)dp[cur][n][i] * dp[cur][l][i]) % MOD;
+    if(l >= 0 && (r + s) % 2 != 1 && (r - s) % 2 != 1 && r != 0) {
+        for(auto i = 0; i <= m; i++) {
+            result = (result + (long long)dp[n][i] * dp[l][i]) % MOD;
+        }
     }
     cout << result;
     return 0;
