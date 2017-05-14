8658f6c357347f19c7a571325e250ef008891ac1
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-14 00:02:46 -0400

    Update The Longest Increasing Subsequence.cpp

--- The Longest Increasing Subsequence/The Longest Increasing Subsequence.cpp
+++ The Longest Increasing Subsequence/The Longest Increasing Subsequence.cpp
@@ -4,24 +4,21 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    int nums[N];
+    int nums[N], dp[N], result = 0;
     for(auto i = 0; cin >> nums[i]; i++) {
     }
-    int dp[N], result = 0;
     dp[0] = nums[0];
     for(auto i = 1; i < N; i++) {
         if(nums[i] > dp[result]) {
             result++;
             dp[result] = nums[i];
-        }
-        else {
+        } else {
             auto low = 0, high = result;
             while(low < high) {
                 auto mid = (low + high) / 2;
                 if(dp[mid] >= nums[i]) {
                     high = mid;
-                }
-                else {
+                } else {
                     low = mid + 1;
                 }
             }
