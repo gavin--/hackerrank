49595218bd962982093058b9cdf26902cc5d1cc3
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-14 17:08:03 -0400

    Update Sherlock and Pairs.cpp

--- Sherlock and Pairs/Sherlock and Pairs.cpp
+++ Sherlock and Pairs/Sherlock and Pairs.cpp
@@ -2,20 +2,19 @@
 #include <iostream>
 using namespace std;
 
-long long C(int n, int k) {
-	auto result = 1ll;
-	for(auto i = 1; i <= k; i++) {
-		result = result * (n - i + 1);
-		result = result / i;
-	}
-	return result;
+long long C(long long n, long long k) {
+    auto result = 1ll;
+    for(auto i = 1; i <= k; i++) {
+        result = result * (n - i + 1);
+        result = result / i;
+    }
+    return result;
 }
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        int N;
-        cin >> N;
+    cin >> T;
+    for(int N; cin >> N; ) {
         map<int, int> A;
         for(auto i = 0; i != N; i++) {
             int j;
