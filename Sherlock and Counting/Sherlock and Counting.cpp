a37c9ff0d5b4adb5f9e401d6a67c5be21c43a7bc
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-14 17:11:23 -0400

    Update Sherlock and Counting.cpp

--- Sherlock and Counting/Sherlock and Counting.cpp
+++ Sherlock and Counting/Sherlock and Counting.cpp
@@ -3,9 +3,8 @@ using namespace std;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        long long n, k;
-        cin >> n >> k;
+    cin >> T;
+    for(long long n, k; cin >> n >> k; cout << endl) {
         auto begin = 1ll, end = n / 2;
         while(begin < end) {
             auto mid = (begin + end + 1) / 2;
@@ -16,11 +15,10 @@ int main() {
             }
         }
         if(end + end == n) {
-            cout << 2 * end - 1;
+            cout << n - 1;
         } else {
             cout << 2 * end;
         }
-        cout << endl;
     }
     return 0;
 }
