e1a68a791a1c89757f30b6571de640e57310df62
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-14 00:09:04 -0400

    Update Taum and B'day.cpp

--- Taum and B-day/Taum and B'day.cpp
+++ Taum and B-day/Taum and B'day.cpp
@@ -3,10 +3,8 @@ using namespace std;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        long long B, W, X, Y, Z;
-        cin >> B >> W >> X >> Y >> Z;
-        cout << B * min(X, Y + Z) + W * min(Y, X + Z) << endl;
+    cin >> T;
+    for(long long B, W, X, Y, Z; cin >> B >> W >> X >> Y >> Z; cout << B * min(X, Y + Z) + W * min(Y, X + Z) << endl) {
     }
     return 0;
 }
