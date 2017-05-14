fc3c97cb9ca88591c5674998f03c55b23c5b70f5
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-14 17:30:17 -0400

    Update Kingdom Division.cpp

--- Kingdom Division/Kingdom Division.cpp
+++ Kingdom Division/Kingdom Division.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <forward_list>
+#include <array>
 using namespace std;
 
 const long long MOD = 1000000007;
 
-void dfs(int current, int parent, pair<long long, long long>* dp, forward_list<int>* graph) {
+void dfs(int current, int parent, array<long long, 2>* dp, forward_list<int>* graph) {
     dp[current] = {0, 1};
     for(auto& i : graph[current]) {
         if(i != parent) {
             dfs(i, current, dp, graph);
-            dp[current] = {(dp[current].first * (dp[i].first * 2 + dp[i].second) + dp[current].second * (dp[i].first + dp[i].second)) % MOD, dp[current].second * dp[i].first % MOD};
+            dp[current] = {(dp[current][0] * (dp[i][0] * 2 + dp[i][1]) + dp[current][1] * (dp[i][0] + dp[i][1])) % MOD, dp[current][1] * dp[i][0] % MOD};
         }
     }
 }
@@ -19,13 +20,11 @@ int main() {
     cin >> n;
     forward_list<int> graph[n];
     for(int i, j; cin >> i >> j; ) {
-        i--;
-        j--;
-        graph[i].emplace_front(j);
-        graph[j].emplace_front(i);
+        graph[i - 1].emplace_front(j - 1);
+        graph[j - 1].emplace_front(i - 1);
     }
-    pair<long long, long long> dp[n];
+    array<long long, 2> dp[n];
     dfs(0, -1, dp, graph);
-    cout << dp[0].first * 2 % MOD;
+    cout << dp[0][0] * 2 % MOD;
     return 0;
 }
