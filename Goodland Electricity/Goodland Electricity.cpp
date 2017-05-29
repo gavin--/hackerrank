ea2865055ea605b71a6f48b5a3b6600d194e855e
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-29 18:04:31 -0400

    Update Goodland Electricity.cpp

--- Goodland Electricity/Goodland Electricity.cpp
+++ Goodland Electricity/Goodland Electricity.cpp
@@ -1,13 +1,12 @@
-#include <iterator>
-#include <vector>
 #include <iostream>
 using namespace std;
 
 int main() {
     int n, k, j = 0, result = 0;
     cin >> n >> k;
-    vector<bool> c(istream_iterator<bool> {cin}, istream_iterator<bool> {});
     for(auto i = 0, last = -1; i != n; i++) {
+        bool c;
+        cin >> c;
         if(i - j >= k) {
             if(last == -1 || last - j >= k) {
                 result = -1;
@@ -16,7 +15,7 @@ int main() {
             j = last + k;
             result++;
         }
-        if(c[i]) {
+        if(c) {
             last = i;
         }
     }
