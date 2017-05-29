611b4dcaf3c45c0a30b2d0b1b0ef65d31951d58f
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-29 17:55:08 -0400

    Update Jumping on the Clouds.cpp

--- Jumping on the Clouds/Jumping on the Clouds.cpp
+++ Jumping on the Clouds/Jumping on the Clouds.cpp
@@ -1,25 +1,18 @@
 #include <vector>
+#include <iterator>
 #include <iostream>
 using namespace std;
 
 int main() {
     int n, result = 0;
     cin >> n;
-    vector<bool> c(n);
-    for(auto i = 0; i != n; i++) {
-        int j;
-        cin >> j;
-        if(j == 1) {
-            c[i] = 1;
-        }
-    }
-    for(auto i = 0; i < n - 1; ) {
+    vector<bool> c(istream_iterator<bool> {cin}, istream_iterator<bool> {});
+    for(auto i = 0; i < n - 1; result++) {
         if(i + 2 < n && c[i + 2] == 0) {
             i += 2;
         } else {
             i++;
         }
-        result++;
     }
     cout << result;
     return 0;
