7b77712cb3067e0a1565b9783dea432953b9d98d
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-05-28 21:50:34 -0400

    Create Birthday Triplets.cpp

---
+++ Birthday Triplets/Birthday Triplets.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+const long long mod = 1000000007;
+
+long long pow(long long x, long long n) {
+    auto result = 1ll;
+    while(n != 0) {
+        if(n % 2 == 1) {
+            result = result * x % mod;
+        }
+        n /= 2;
+        x = x * x % mod;
+    }
+    return result;
+}
+
+long long sqrt(long long n) {
+    auto begin = 1ll, end = n / 2;
+    while(begin < end) {
+        auto mid = (end + begin) / 2;
+        if(mid < n / mid) {
+            begin = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+    return end;
+}
+
+long long sum(long long a, long long l, long long r) {
+	if(a == 1) {
+		return (r - l + 1) % mod;
+	}
+	return pow(a, l) * (pow(a, r - l + 1) - 1) % mod * pow(a - 1, mod - 2) % mod;
+}
+
+int main() {
+    int q;
+    cin >> q;
+    for(long long f2, f3, f4, l, r; cin >> f2 >> f3 >> f4 >> l >> r; ) {
+        for(auto a = 1ll; ; a++) {
+            auto a2 = a * a, a3 = a2 * a, a4 = a2 * a2;
+            auto r2 = f2 - a2, r3 = f3 - a3, r4 = f4 - a4;
+            auto temp = 2 * r4 - r2 * r2;
+            auto discriminant = sqrt(temp);
+            if(discriminant * discriminant != temp) {
+                continue;
+            }
+            long long roots[2] = {sqrt((r2 + discriminant) / 2), sqrt((r2 - discriminant) / 2)};
+            for(auto i = 0; i <= 1; i++) {
+                auto b = roots[i], c = roots[1 - i];
+                auto b2 = b * b, b3 = b2 * b, b4 = b * b3;
+                auto c2 = c * c, c3 = c2 * c, c4 = c * c3;
+                if(a < b < c && a2 + b2 + c2 == f2 && a3 + b3 + c3 == f3 && a4 + b4 + c4 == f4) {
+                    auto s = (sum(a, l, r) + sum(b, l, r) + sum(c, l, r)) % mod;
+                    cout << s << endl;
+                    goto next;
+                }
+            }
+        }
+        next:
+            ;
+    }
+    return 0;
+}
