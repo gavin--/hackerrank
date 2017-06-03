0f891dcdd40bb71321930b6cd58ed7a92a1b30f1
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-06-02 22:55:27 -0400

    Update Chessboard Game, Again!.cpp

--- Chessboard Game- Again/Chessboard Game, Again!.cpp
+++ Chessboard Game- Again/Chessboard Game, Again!.cpp
@@ -1,52 +1,60 @@
 #include <iostream>
-#include <set>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 char grundy[15][15];
 
 int mex(int x, int y) {
-    set<int> set;
+    vector<int> set;
     if(x - 2 >= 0) {
         if(y - 1 >= 0) {
             if(grundy[x - 2][y - 1] != -1) {
-                set.emplace(grundy[x - 2][y - 1]);
+                set.emplace_back(grundy[x - 2][y - 1]);
             } else {
-                set.emplace(mex(x - 2, y - 1));
+                set.emplace_back(mex(x - 2, y - 1));
             }
         }
         if(y < 14) {
             if(grundy[x - 2][y + 1] != -1) {
-                set.emplace(grundy[x - 2][y + 1]);
+                set.emplace_back(grundy[x - 2][y + 1]);
             } else {
-                set.emplace(mex(x - 2, y + 1));
+                set.emplace_back(mex(x - 2, y + 1));
             }
         }
     }
     if(y - 2 >= 0) {
         if(x < 14) {
             if(grundy[x + 1][y - 2] != -1) {
-                set.emplace(grundy[x + 1][y - 2]);
+                set.emplace_back(grundy[x + 1][y - 2]);
             } else {
-                set.emplace(mex(x + 1, y - 2));
+                set.emplace_back(mex(x + 1, y - 2));
             }
         }
         if(x - 1 >= 0) {
             if(grundy[x - 1][y - 2] != -1) {
-                set.emplace(grundy[x - 1][y - 2]);
+                set.emplace_back(grundy[x - 1][y - 2]);
             } else {
-                set.emplace(mex(x - 1, y - 2));
+                set.emplace_back(mex(x - 1, y - 2));
             }
         }
     }
     auto result = 0;
-    while(set.count(result) == 1) {
-        result++;
+    sort(begin(set), end(set));
+    if(set.empty() || set[0] != 0) {
+        return 0;
     }
-    return result;
+    for(auto i = 1; i < set.size(); i++) {
+        if(set[i] != set[i - 1] + 1 && set[i] != set[i - 1]) {
+            return set[i - 1] + 1;
+        }
+    }
+    return set[set.size() - 1] + 1;
 }
 
 int main() {
     int T;
+    cin >> T;
     for(auto i = 0; i < 15; i++) {
         fill(begin(grundy[i]), end(grundy[i]), -1);
     }
@@ -55,9 +63,8 @@ int main() {
             grundy[i][j] = mex(i, j);
         }
     }
-    for(cin >> T; T > 0; T--) {
-        int k, result = 0;
-        cin >> k;
+    for(int k; cin >> k; ) {
+        auto result = 0;
         while(k > 0) {
             int x, y;
             cin >> x >> y;
