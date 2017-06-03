cb962b911cf6c58aa6804525997c1f8559806e29
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-06-02 22:58:19 -0400

    Update No Prefix Set.cpp

--- No Prefix Set/No Prefix Set.cpp
+++ No Prefix Set/No Prefix Set.cpp
@@ -10,13 +10,13 @@ int main() {
     string result = "GOOD SET";
     for(string s; cin >> s; ) {
         auto find1 = set1.lower_bound(s), find2 = set2.lower_bound(s);
-        if(find1 == set1.end() || (*find1).substr(0, s.size()) != s) {
+        if(find1 == set1.end() || (*find1).compare(0, s.size(), s) != 0) {
             set1.emplace(s);
         } else {
             result = "BAD SET\n" + s;
             break;
         }
-        if(find2 == set2.end() || s.substr(0, (*find2).size()) != *find2) {
+        if(find2 == set2.end() || s.compare(0, (*find2).size(), *find2) != 0) {
             set2.emplace(s);
         } else {
             result = "BAD SET\n" + s;
