ab50dcec3a4e22e34873eb70e6f0fad029bdcaae
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-06-05 14:16:14 -0400

    Update Maximum Draws.cpp

--- Maximum Draws/Maximum Draws.cpp
+++ Maximum Draws/Maximum Draws.cpp
@@ -3,10 +3,8 @@ using namespace std;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        int N;
-        cin >> N;
-        cout << N + 1 << endl;
+    cin >> T;
+    for(int N; cin >> N; cout << N + 1 << endl) {
     }
     return 0;
 }
