6083613e3e67dc5baad86e64057f7a21c0599c08
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-06-05 14:17:27 -0400

    Update Handshake.cpp

--- Handshake/Handshake.cpp
+++ Handshake/Handshake.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-
 using namespace std;
-
 
 int main(){
     int T;
-    for(cin >> T; T > 0; T--){
-        int N;
-        cin >> N;
-        cout << N * (N - 1) / 2 << endl;
+    cin >> T;
+    for(int N; cin >> N; cout << N * (N - 1) / 2 << endl) {
     }
     return 0;
 }
