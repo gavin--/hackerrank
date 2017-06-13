5b98dc79a3f962aa76bfb3daf08f72e9f18d5d06
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-06-13 13:02:46 -0400

    Create Difference and Product.cpp

--- Difference and Product/Difference and Product.cpp
+++ Difference and Product/Difference and Product.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 long long sqrt(long long n) {
-    auto begin = 0ll, end = n / 2 + 1;
+    auto begin = 0ll, end = n;
     while(begin < end) {
         auto mid = (end + begin + 1) / 2;
         if(mid <= n / mid) {
@@ -36,10 +36,10 @@ int main() {
                 }
             }
         } else if(D > 0) {
-            auto discriminant = D * D + 4 * P, t = sqrt(discriminant);
+            auto discriminant = D * D + 4 * P, root = sqrt(discriminant);
             if(discriminant == 0) {
                 result = 2;
-            } else if(t * t == discriminant && (t - D) % 2 == 0) {
+            } else if(root * root == discriminant && (root - D) % 2 == 0) {
                 result = 4;
             }
         }
