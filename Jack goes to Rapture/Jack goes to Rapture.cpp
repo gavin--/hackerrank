f6afecd59e0e45160e0d7e13d252a237a119f87a
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-06-17 15:07:45 -0400

    Update Jack goes to Rapture.cpp

--- Jack goes to Rapture/Jack goes to Rapture.cpp
+++ Jack goes to Rapture/Jack goes to Rapture.cpp
@@ -10,11 +10,11 @@ int main() {
     forward_list<pair<int, int>> graph[N];
     int dist[N];
     dist[0] = 0;
-    set<pair<int, int>> s;
-    s.emplace(dist[0], 0);
+    set<pair<int, int>> set;
+    set.emplace(dist[0], 0);
     for(auto i = 1; i != N; i++) {
         dist[i] = INT_MAX;
-        s.emplace(dist[i], i);
+        set.emplace(dist[i], i);
     }
     for(int a, b, c; cin >> a >> b >> c; ) {
         a--;
@@ -22,18 +22,18 @@ int main() {
         graph[a].emplace_front(b, c);
         graph[b].emplace_front(a, c);
     }
-    while(!s.empty()) {
-        auto current = s.begin()->second;
-        s.erase(s.begin());
+    do {
+        auto current = set.begin()->second;
+        set.erase(set.begin());
         for(auto& i : graph[current]) {
             auto d = max(i.second - dist[current], 0) + dist[current];
             if(dist[i.first] > d) {
-                s.erase({dist[i.first], i.first});
+                set.erase({dist[i.first], i.first});
                 dist[i.first] = d;
-                s.emplace(dist[i.first], i.first);
+                set.emplace(dist[i.first], i.first);
             }
         }
-    }
+    } while(!set.empty());
     if(dist[N - 1] == INT_MAX) {
         cout << "NO PATH EXISTS";
     } else {
