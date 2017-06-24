4cdebff85d4ece885339b6b7fe9d454f44b4461e
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-06-24 19:44:54 -0400

    Update Even Tree.cpp

--- Even Tree/Even Tree.cpp
+++ Even Tree/Even Tree.cpp
@@ -1,16 +1,12 @@
 #include <forward_list>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-int result = 0;
-
-int dfs(int node, int parent, forward_list<int>* graph, int N) {
+int dfs(int node, int parent, forward_list<int>* graph, int& result) {
     auto current = 0;
     for(auto& i : graph[node]) {
         if(i != parent) {
-            auto temp = dfs(i, node, graph, N) + 1;
+            auto temp = dfs(i, node, graph, result) + 1;
             if(temp % 2 == 0) {
                 result++;
             }
@@ -21,15 +17,14 @@ int dfs(int node, int parent, forward_list<int>* graph, int N) {
 }
 
 int main() {
-    int N, M;
+    int N, M, result = 0;
     cin >> N >> M;
     forward_list<int> graph[N];
-    fill(graph, graph + N, forward_list<int>());
     for(int a, b; cin >> a >> b; ) {
         graph[a - 1].emplace_front(b - 1);
         graph[b - 1].emplace_front(a - 1);
     }
-    dfs(0, -1, graph, N);
+    dfs(0, -1, graph, result);
     cout << result;
     return 0;
 }
