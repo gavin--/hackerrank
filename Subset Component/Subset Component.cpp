c17239ca2ae6f7c080ba8147553b72001e63ed7a
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-06-27 12:52:31 -0400

    Create Subset Component.cpp

---
+++ Subset Component/Subset Component.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    unsigned long long d[n];
+    for(auto i = 0; cin >> d[i]; i++) {
+    }
+    auto result = 64;
+    for(auto i = 1ull, size = 1ull << n; i < size; i++) {
+        vector<unsigned long long> v;
+        for(auto j = 0; j < n; j++) {
+            if((1ull << j) & i) {
+                v.emplace_back(d[j]);
+            }
+        }
+        auto mask = 0ull;
+        for(auto i = 0; i < v.size(); i++) {
+            for(auto j = i + 1; j < v.size(); j++) {
+                if(v[i] & v[j]) {
+                    v[j] = v[i] | v[j];
+                    v[i] = 0;
+                    break;
+                }
+            }
+            mask |= v[i];
+            if(v[i] != 0) {
+                result++;
+            }
+        }
+        for(auto i = 0; i < 64; i++) {
+            if((mask & (1ull << i)) == 0) {
+                result++;
+            }
+        }
+    }
+    cout << result;
+    return 0;
+}
