d0584ac3eeaa0ca47163599dfbcb8add7cba0f42
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-06-27 14:03:43 -0400

    Update Easy sum.cpp

--- Easy sum/Easy sum.cpp
+++ Easy sum/Easy sum.cpp
@@ -3,12 +3,8 @@ using namespace std;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        long long N, m, result = 0;
-        cin >> N >> m;
-        result += N / m * m * (m - 1) / 2;
-        result += N % m * (N % m + 1) / 2;
-        cout << result << endl;
+    cin >> T;
+    for(long long N, m; cin >> N >> m; cout << N / m * m * (m - 1) / 2 + N % m * (N % m + 1) / 2 << endl) {
     }
     return 0;
 }
