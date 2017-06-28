8b6c36b3900a1ef9fdfc38e24e8cec821a3b4376
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-06-28 19:36:10 -0400

    Update Save the Prisoner!.cpp

--- Save the Prisoner/Save the Prisoner!.cpp
+++ Save the Prisoner/Save the Prisoner!.cpp
@@ -3,9 +3,8 @@ using namespace std;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        int N, M, S;
-        cin >> N >> M >> S;
+    cin >> T;
+    for(int N, M, S; cin >> N >> M >> S; ) {
         auto result = (M + S - 1) % N;
         if(result == 0) {
             result = N;
