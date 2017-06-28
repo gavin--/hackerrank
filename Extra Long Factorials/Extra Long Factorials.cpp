ca44a3de8162d16f87dbfe675adbbb539bb85ebd
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-06-28 00:55:45 -0400

    Update and rename Extra Long Factorials.java to Extra Long Factorials.cpp

---
+++ Extra Long Factorials/Extra Long Factorials.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string multiply(const string& a, const string& b) {
+    int product[a.size() + b.size()];
+    fill(product, product + a.size() + b.size(), 0);
+    for(auto i = 0; i < a.size(); i++) {
+        for(auto j = 0; j < b.size(); j++) {
+            product[i + j] += (a.rbegin()[i] - '0') * (b.rbegin()[j] - '0');
+        }
+    }
+    auto size = 0;
+    for(auto i = 0, j = 0; i < a.size() + b.size(); i++) {
+        auto t = product[i] + j;
+        product[i] = (product[i] + j) % 10;
+        j = t / 10;
+        if(product[i] != 0) {
+            size = i + 1;
+        }
+    }
+    if(size == 0) {
+        return "0";
+    }
+    string result(size, '0');
+    for(auto i = 0; i < size; i++) {
+        result.rbegin()[i] = product[i] + '0';
+    }
+    return result;
+}
+
+int main() {
+    int N;
+    string result = "1";
+    for(cin >> N; N > 1; N--) {
+        result = multiply(result, to_string(N));
+    }
+    cout << result;
+    return 0;
+}
