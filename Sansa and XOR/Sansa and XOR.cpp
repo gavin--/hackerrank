ad2c568accee59f58b0e03f90e41a5f61d78728b
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-07-08 16:12:38 -0400

    Update Sansa and XOR.cpp

--- Sansa and XOR/Sansa and XOR.cpp
+++ Sansa and XOR/Sansa and XOR.cpp
@@ -3,11 +3,10 @@ using namespace std;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        int N, result = 0;
-        cin >> N;
-        for(auto i = 0; i != N; i++) {
-            int a;
+    cin >> T;
+    for(int N; cin >> N; ) {
+        int result = 0;
+        for(int i = 0, a; i != N; i++) {
             cin >> a;
             if((i + 1) % 2 * (N - i) % 2 == 1) {
                 result = result ^ a;
