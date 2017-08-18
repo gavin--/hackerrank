91a79c4a6608337e2ee9d9515bab228d624a2cdc
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-08-18 12:19:18 -0400

    Update Interval Selection.cpp

--- Interval Selection/Interval Selection.cpp
+++ Interval Selection/Interval Selection.cpp
@@ -1,36 +1,30 @@
-#include <list>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 int main() {
     int s;
-    for(cin >> s; s > 0; s--) {
-        int n;
-        cin >> n;
+    cin >> s;
+    for(int n; cin >> n; ) {
         pair<int, int> intervals[n];
-        for(auto i = 0; i != n; i++) {
+        for(int i = 0; i < n; i++) {
             cin >> intervals[i].first >> intervals[i].second;
         }
-        sort(intervals, intervals + n);
-        list<pair<int, int>> list;
-        for(auto& i : intervals) {
-            list.emplace_back(i);
-            auto count = 0;
-            auto k = list.end();
-            for(auto j = list.begin(); j != list.end(); j++) {
-                if(j->second >= i.first) {
-                    count++;
-                    if(k == list.end() || k->second < j->second) {
-                        k = j;
-                    }
-                }
-            }
-            if(count == 3) {
-                list.erase(k);
+        sort(intervals, intervals + n, [](pair<int, int>& left, pair<int, int>& right) {
+           return left.second < right.second; 
+        });
+        int result = 0;
+        for(int i = 0, end = 0, prev_end = 0; i < n; i++) {
+            if(intervals[i].first > end) {
+                end = intervals[i].second;
+                result++;
+            } else if(intervals[i].first > prev_end) {
+                prev_end = end;
+                end = intervals[i].second;
+                result++;
             }
         }
-        cout << list.size() << endl;
+        cout << result << endl;
     }
     return 0;
 }
