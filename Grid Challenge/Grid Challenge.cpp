306a996ee19dc344634e0231e54855c5fc0c0778
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-09-08 21:14:01 -0400

    Update Grid Challenge.cpp

--- Grid Challenge/Grid Challenge.cpp
+++ Grid Challenge/Grid Challenge.cpp
@@ -4,17 +4,16 @@ using namespace std;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        int N;
-        cin >> N;
+    cin >> T;
+    for(int N; cin >> N; ) {
         string G[N];
-        for(auto i = 0; i != N; i++) {
+        for(int i = 0; i != N; i++) {
             cin >> G[i];
             sort(begin(G[i]), end(G[i]));
         }
         auto result = true;
-        for(auto i = 0; i != N; i++) {
-            for(auto j = 1; j != N; j++) {
+        for(int i = 0; i != N; i++) {
+            for(int j = 1; j != N; j++) {
                 if(G[j][i] < G[j - 1][i]) {
                     result = false;
                 }
