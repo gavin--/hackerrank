de79dd707d88923c3c245489558564b5629544a6
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-10-19 19:46:20 -0400

    Create Divisor Exploration.cpp

---
+++ Divisor Exploration/Divisor Exploration.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+const long long MOD = 1000000007, INV = 500000004, SIZE = 200001;
+
+long long pow(long long x, long long n) {
+    long long result = 1;
+    while(n != 0) {
+        if(n % 2 == 1) {
+            result = result * x % MOD;
+        }
+        x = x * x % MOD;
+        n /= 2;
+    }
+    return result;
+}
+
+int main() {
+	int D;
+	cin >> D;
+	long long factorial[SIZE], ifactorial[SIZE], inv[SIZE];
+	inv[1] = 1;
+	factorial[0] = 1;
+	factorial[1] = 1;
+	ifactorial[0] = 1;
+	ifactorial[1] = 1;
+	for(long long i = 2; i < SIZE; i++) {
+	    inv[i] = (-MOD / i + MOD) * inv[MOD % i] % MOD;
+	    factorial[i] = factorial[i - 1] * i % MOD;
+	    ifactorial[i] = ifactorial[i - 1] * inv[i] % MOD;
+	}
+	for(long long m, a; cin >> m >> a; ) {
+	    long long result = (a + 2) * (a + m + 2) % MOD;
+	    long long t = factorial[a + m + 1] * ifactorial[a + 2] % MOD;
+	    result = result * t % MOD * t % MOD;
+	    cout << result * pow(INV, m) % MOD << endl;
+	}
+	return 0;
+}
