c47f32cde30ea0f94c1c476dc045e9dc19c09fb4
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-10-19 20:00:02 -0400

    Update Divisor Exploration.cpp

--- Divisor Exploration/Divisor Exploration.cpp
+++ Divisor Exploration/Divisor Exploration.cpp
@@ -16,24 +16,24 @@ long long pow(long long x, long long n) {
 }
 
 int main() {
-	int D;
-	cin >> D;
-	long long factorial[SIZE], ifactorial[SIZE], inv[SIZE];
-	inv[1] = 1;
-	factorial[0] = 1;
-	factorial[1] = 1;
-	ifactorial[0] = 1;
-	ifactorial[1] = 1;
-	for(long long i = 2; i < SIZE; i++) {
-	    inv[i] = (-MOD / i + MOD) * inv[MOD % i] % MOD;
-	    factorial[i] = factorial[i - 1] * i % MOD;
-	    ifactorial[i] = ifactorial[i - 1] * inv[i] % MOD;
-	}
-	for(long long m, a; cin >> m >> a; ) {
-	    long long result = (a + 2) * (a + m + 2) % MOD;
-	    long long t = factorial[a + m + 1] * ifactorial[a + 2] % MOD;
-	    result = result * t % MOD * t % MOD;
-	    cout << result * pow(INV, m) % MOD << endl;
-	}
-	return 0;
+    int D;
+    cin >> D;
+    long long factorial[SIZE], ifactorial[SIZE], inv[SIZE];
+    inv[1] = 1;
+    factorial[0] = 1;
+    factorial[1] = 1;
+    ifactorial[0] = 1;
+    ifactorial[1] = 1;
+    for(long long i = 2; i < SIZE; i++) {
+        inv[i] = (-MOD / i + MOD) * inv[MOD % i] % MOD;
+        factorial[i] = factorial[i - 1] * i % MOD;
+        ifactorial[i] = ifactorial[i - 1] * inv[i] % MOD;
+    }
+    for(long long m, a; cin >> m >> a; ) {
+        long long result = (a + 2) * (a + m + 2) % MOD;
+        long long t = factorial[a + m + 1] * ifactorial[a + 2] % MOD;
+        result = result * t % MOD * t % MOD;
+        cout << result * pow(INV, m) % MOD << endl;
+    }
+    return 0;
 }
