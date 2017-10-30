6b9d63de1be8ba44447c2ea9b4b80e692f78f39a
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-10-30 14:00:30 -0400

    Update Mandragora Forest.cpp

--- Mandragora Forest/Mandragora Forest.cpp
+++ Mandragora Forest/Mandragora Forest.cpp
@@ -4,17 +4,16 @@ using namespace std;
 
 int main() {
     int T;
-    for(cin >> T; T > 0; T--) {
-        int N;
-        cin >> N;
+    cin >> T;
+    for(int N; cin >> N; T--) {
         int H[N];
-        auto sum = 0ll;
+        long long sum = 0;
         for(auto i = 0; i != N; i++) {
             cin >> H[i];
             sum += H[i];
         }
         sort(H, H + N);
-        auto S = 1ll, P = sum;
+        long long S = 1, P = sum;
         for(auto i = 0; i != N; i++) {
             S++;
             sum -= H[i];
