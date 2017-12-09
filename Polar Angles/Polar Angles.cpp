b7866241a4cf2f021c557c68fcd26ab90d5655e1
gavin-- <gavinzhubbb4mp5@gmail.com>
2017-12-09 15:23:11 -0500

    Update Polar Angles.cpp

--- Polar Angles/Polar Angles.cpp
+++ Polar Angles/Polar Angles.cpp
@@ -1,35 +1,48 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
-    vector<pair<int, int>> points[4];
-    for(int x, y; cin >> x >> y; ) {
-        if(x > 0 && y >= 0) {
-            points[0].emplace_back(x, y);
-        } else if(x <= 0 && y > 0) {
-            points[1].emplace_back(x, y);
-        } else if(x < 0 && y <= 0) {
-            points[2].emplace_back(x, y);
-        } else {
-            points[3].emplace_back(x, y);
-        }
+    int n;
+    cin >> n;
+    pair<int, int> points[n];
+    for(int i = 0; cin >> points[i].first >> points[i].second; i++) {
     }
     auto polar = [] (pair<int, int>& p1, pair<int, int>& p2) {
-        auto left = p1.second * p2.first, right = p2.second * p1.first;
+        int x1 = p1.first, y1 = p1.second, x2 = p2.first, y2 = p2.second;
+        if(y1 == 0) {
+            if(x1 > 0) {
+                if(y2 == 0 && x2 < x1 && x2 > 0) {
+                    return false;
+                }
+                return true;
+            }
+            if(y2 < 0 || (y2 == 0 && x2 < x1)) {
+                return true;
+            }
+            return false;
+        }
+        if(y2 == 0) {
+            if(y1 > 0 && x2 < 0) {
+                return true;
+            }
+            return false;
+        }
+        if(y1 > 0 && y2 < 0) {
+            return true;
+        }
+        if(y1 < 0 && y2 > 0) {
+            return false;
+        }
+        int left = y1 * x2, right = y2 * x1;
         if(left == right) {
-            return p1.first * p1.first + p1.second * p1.second < p2.first * p2.first + p2.second * p2.second;
+            return x1 * x1 + y1 * y1 < x2 * x2 + y2 * y2;
         }
         return left < right;
     };
+    sort(points, points + n, polar);
     for(auto& i : points) {
-        sort(begin(i), end(i), polar);
-        for(auto& j : i) {
-            cout << j.first << ' ' << j.second << endl;
-        }
+        cout << i.first << ' ' << i.second << endl;
     }
     return 0;
 }
