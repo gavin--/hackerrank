c035e447d9c80518abdeb65f7b999128b48c547d
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-07-31 12:19:37 -0400

    Update Running Time of Algorithms.cpp

--- Running Time of Algorithms/Running Time of Algorithms.cpp
+++ Running Time of Algorithms/Running Time of Algorithms.cpp
@@ -1,28 +1,5 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
-const int SIZE = 10001;
-
-int lowbit(int i) {
-    return i & -i;
-}
-
-int get(vector<int>& bit, int index) {
-    int result = 0;
-    while(index > 0) {
-        result += bit[index];
-        index -= lowbit(index);
-    }
-    return result;
-}
-
-void add(vector<int>& bit, int index, int val) {
-    while(index < bit.size()) {
-        bit[index] += val;
-        index += lowbit(index);
-    }
-}
 
 int main() {
     int n;
@@ -30,10 +7,12 @@ int main() {
     int a[n], result = 0;
     for(int i = 0; cin >> a[i]; i++) {
     }
-    vector<int> bit(SIZE, 0);
-    for(int i = n - 1; i >= 0; i--) {
-        result += get(bit, a[i] - 1);
-        add(bit, a[i], 1);
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(a[i] > a[j]) {
+                result++;
+            }
+        }
     }
     cout << result;
     return 0;
