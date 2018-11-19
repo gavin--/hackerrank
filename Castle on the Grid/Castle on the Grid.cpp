c27bac99fd312112be496db6f773c074556b73dc
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-11-19 12:49:01 -0500

    Update Castle on the Grid.cpp

--- Castle on the Grid/Castle on the Grid.cpp
+++ Castle on the Grid/Castle on the Grid.cpp
@@ -3,34 +3,29 @@
 #include <algorithm>
 using namespace std;
 
-vector<vector<vector<pair<int, int>>>> make_graph(int N) {
-    vector<bool> grid[N];
-    for(auto i = 0; i < N; i++) {
-        grid[i] = vector<bool>();
-        for(auto j = 0; j < N; j++) {
+vector<vector<vector<pair<int, int>>>> make_graph(int n) {
+    vector<bool> grid[n];
+    for(int i = 0; i < n; i++) {
+        for(auto j = 0; j < n; j++) {
             char c;
             cin >> c;
-            if(c == '.') {
-                grid[i].emplace_back(true);
-            } else {
-                grid[i].emplace_back(false);
-            }
+            grid[i].emplace_back(c == '.');
         }
     }
-    vector<vector<vector<pair<int, int>>>> graph(N, vector<vector<pair<int, int>>>(N));
-    for(auto i = 0; i < N; i++) {
-        for(auto j = 0; j < N; j++) {
+    vector<vector<vector<pair<int, int>>>> graph(n, vector<vector<pair<int, int>>>(n));
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
             graph[i][j] = vector<pair<int, int>>();
-            for(auto k = i + 1; k < N && grid[k][j]; k++) {
+            for(int k = i + 1; k < n && grid[k][j]; k++) {
                 graph[i][j].emplace_back(k, j);
             }
-            for(auto k = i - 1; k >= 0 && grid[k][j]; k--) {
+            for(int k = i - 1; k >= 0 && grid[k][j]; k--) {
                 graph[i][j].emplace_back(k, j);
             }
-            for(auto k = j + 1; k < N && grid[i][k]; k++) {
+            for(int k = j + 1; k < n && grid[i][k]; k++) {
                 graph[i][j].emplace_back(i, k);
             }
-            for(auto k = j - 1; k >= 0 && grid[i][k]; k--) {
+            for(int k = j - 1; k >= 0 && grid[i][k]; k--) {
                 graph[i][j].emplace_back(i, k);
             }
         }
@@ -39,30 +34,31 @@ vector<vector<vector<pair<int, int>>>> make_graph(int N) {
 }
 
 int main() {
-    int N, a, b, c, d;
-    cin >> N;
-    auto graph = make_graph(N);
+    int n, a, b, c, d;
+    cin >> n;
+    auto graph = make_graph(n);
     cin >> a >> b >> c >> d;
-    vector<bool> visited[N];
-    fill(visited, visited + N, vector<bool>(N, false));
-    queue<pair<int, int>> queue;
-    queue.emplace(a, b);
-    visited[a][b] = true;
-    for(auto result = 0; !queue.empty(); result++) {
-        for(auto size = queue.size(); size > 0; size--) {
-            auto front = queue.front();
-            if(front.first == c && front.second == d) {
+    vector<vector<bool>> visited1(n, vector<bool>(n)), visited2(n, vector<bool>(n));
+    queue<pair<int, int>> queue1, queue2;
+    queue1.emplace(a, b);
+    queue2.emplace(c, d);
+    visited1[a][b] = true;
+    for(int result = 0; !queue1.empty() && !queue2.empty(); result++) {
+        for(auto size = queue1.size(); size > 0; size--) {
+            if(visited2[queue1.front().first][queue1.front().second]) {
                 cout << result;
                 return 0;
             }
-            queue.pop();
-            for(auto& i : graph[front.first][front.second]) {
-                if(!visited[i.first][i.second]) {
-                    queue.emplace(i.first, i.second);
-                    visited[i.first][i.second] = true;
+            for(auto& i : graph[queue1.front().first][queue1.front().second]) {
+                if(!visited1[i.first][i.second]) {
+                    queue1.emplace(i.first, i.second);
+                    visited1[i.first][i.second] = true;
                 }
             }
+            queue1.pop();
         }
+        swap(queue1, queue2);
+        swap(visited1, visited2);
     }
     return 0;
 }
