d777bf57fcebfae41e7d8f7ad6ba1c58aa2fabe6
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-11-19 17:07:59 -0500

    Update Down to Zero II.cpp

--- Down to Zero II/Down to Zero II.cpp
+++ Down to Zero II/Down to Zero II.cpp
@@ -3,28 +3,26 @@
 using namespace std;
 
 int main() {
-    int Q, max = 0;
-    cin >> Q;
-    int queries[Q];
-    for(auto i = 0; cin >> queries[i]; i++) {
-        if(queries[i] > max) {
-            max = queries[i];
-        }
+    int q, size = 0;
+    cin >> q;
+    int queries[q];
+    for(int i = 0; cin >> queries[i]; i++) {
+        size = max(size, queries[i]);
     }
-    short dp[max + 1];
+    int dp[size + 1];
     dp[0] = 0;
-    fill(dp + 1, dp + max + 1, SHRT_MAX);
-    for(auto i = 1; i <= max; i++) {
+    fill(dp + 1, dp + size + 1, INT_MAX);
+    for(int i = 1; i <= size; i++) {
         if(dp[i - 1] < dp[i]) {
             dp[i] = dp[i - 1] + 1;
         }
-        for(auto j = 2; j <= i && i * j <= max; j++) {
+        for(int j = 2; j <= i && i * j <= size; j++) {
             if(dp[i] < dp[i * j]) {
                 dp[i * j] = dp[i] + 1;
             }
         }
     }
-    for(auto i = 0; i != Q; i++) {
+    for(int i = 0; i != q; i++) {
         cout << dp[queries[i]] << endl;
     }
     return 0;
