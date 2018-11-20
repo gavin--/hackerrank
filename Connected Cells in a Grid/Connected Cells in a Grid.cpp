44aefa6cac9af4397c48074de53d044727cfd893
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-11-19 19:48:44 -0500

    Update Connected Cells in a Grid.cpp

--- Connected Cells in a Grid/Connected Cells in a Grid.cpp
+++ Connected Cells in a Grid/Connected Cells in a Grid.cpp
@@ -1,23 +1,18 @@
 #include <stack>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int main() {
     int n, m, result = 0;
     cin >> n >> m;
-    vector<bool> x[n];
-    fill(x, x + n, vector<bool>(m));
-    for(auto i = 0; i != n; i++) {
-        for(auto j = 0; j != m; j++) {
-            bool k;
-            cin >> k;
-            x[i][j] = k;
+    bool x[n][m];
+    for(int i = 0; i != n; i++) {
+        for(int j = 0; j != m; j++) {
+            cin >> x[i][j];
         }
     }
-    for(auto i = 0; i != n; i++) {
-        for(auto j = 0; j != m; j++) {
+    for(int i = 0; i != n; i++) {
+        for(int j = 0; j != m; j++) {
             if(x[i][j]) {
                 auto current = 0;
                 x[i][j] = false;
@@ -30,7 +25,7 @@ int main() {
                     result = max(result, current);
                     for(auto k = -1; k <= 1; k++) {
                         for(auto l = -1; l <= 1; l++) {
-                            if(top.first + k >= 0 && top.first + k < n && top.second + l >= 0 && top.second < m) {
+                            if(top.first + k >= 0 && top.first + k < n && top.second + l >= 0 && top.second + l < m) {
                                 if(x[top.first + k][top.second + l]) {
                                     x[top.first + k][top.second + l] = false;
                                     stack.emplace(top.first + k, top.second + l);
