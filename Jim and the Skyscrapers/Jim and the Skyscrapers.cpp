71206e5e9857effb61f3493954daff26a2ef6d99
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-11-29 15:10:05 -0500

    Update Jim and the Skyscrapers.cpp

--- Jim and the Skyscrapers/Jim and the Skyscrapers.cpp
+++ Jim and the Skyscrapers/Jim and the Skyscrapers.cpp
@@ -3,29 +3,25 @@
 using namespace std;
 
 int main() {
-    int N;
-    auto result = 0ll;
-    cin >> N;
+    int n;
+    long long result = 0;
+    cin >> n;
     stack<int> stack;
     for(int h; cin >> h; ) {
-        if(stack.empty() || h < stack.top()) {
-            stack.emplace(h);
-        } else {
-            while(!stack.empty() && h > stack.top()) {
-                auto same = 0ll;
-                auto last = stack.top();
-                while(!stack.empty() && last == stack.top()) {
-                    same++;
-                    stack.pop();
-                }
-                result += same * (same - 1);
+        while(!stack.empty() && h > stack.top()) {
+            long long same = 0;
+            int last = stack.top();
+            while(!stack.empty() && last == stack.top()) {
+                same++;
+                stack.pop();
             }
-            stack.emplace(h);
+            result += same * (same - 1);
         }
+        stack.emplace(h);
     }
     while(!stack.empty()) {
-        auto same = 0ll;
-        auto last = stack.top();
+        long long same = 0;
+        int last = stack.top();
         while(!stack.empty() && last == stack.top()) {
             same++;
             stack.pop();
