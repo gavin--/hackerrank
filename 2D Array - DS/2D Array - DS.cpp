504ccef33ca6581a510f88d98b018a1499de9f8a
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-12-04 12:32:05 -0500

    Update 2D Array - DS.cpp

--- 2D Array - DS/2D Array - DS.cpp
+++ 2D Array - DS/2D Array - DS.cpp
@@ -3,17 +3,15 @@ using namespace std;
 
 int main(){
     int A[6][6], result = -9 * 7;
-    for(auto i = 0; i < 6; i++) {
-        for(auto j = 0; j < 6; j++) {
+    for(int i = 0; i < 6; i++) {
+        for(int j = 0; j < 6; j++) {
             cin >> A[i][j];
-        }
-    }
-    for(auto i = 0; i < 4; i++) {
-        for(auto j = 0; j < 4; j++) {
-            auto sum = A[i][j] + A[i][j + 1] + A[i][j + 2];
-            sum += A[i + 2][j] + A[i + 2][j + 1] + A[i + 2][j + 2];
-            sum += A[i + 1][j + 1];
-            result = max(result, sum);
+            if(i >= 2 && j >= 2) {
+                int sum = A[i - 2][j - 2] + A[i - 2][j - 1] + A[i - 2][j];
+                sum += A[i][j - 2] + A[i][j - 1] + A[i][j];
+                sum += A[i - 1][j - 1];
+                result = max(result, sum);
+            }
         }
     }
     cout << result;
