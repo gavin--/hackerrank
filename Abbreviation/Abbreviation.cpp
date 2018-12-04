9889d0f8d45d14ef788e96cabe1eeec2b4fb39ff
gavin-- <gavinzhubbb4mp5@gmail.com>
2018-12-04 12:46:48 -0500

    Update Abbreviation.cpp

--- Abbreviation/Abbreviation.cpp
+++ Abbreviation/Abbreviation.cpp
@@ -10,13 +10,13 @@ int main() {
         for(auto i = 1; i <= a.size(); i++) {
             dp[0][i] = islower(a[i - 1]);
         }
-        for(auto i = 1; i <= b.size(); i++) {
+        for(int i = 1; i <= b.size(); i++) {
             swap(cur, pre);
             dp[cur][i - 1] = 0;
-            for(auto j = i; j <= a.size(); j++) {
+            for(int j = i; j <= a.size(); j++) {
                 if(islower(a[j - 1])) {
                     dp[cur][j] = dp[cur][j - 1];
-                    if(toupper(a[j - 1]) == b[i - 1] || a[j - 1] == b[i - 1]) {
+                    if(a[j - 1] == tolower(b[i - 1])) {
                         dp[cur][j] = dp[cur][j] || dp[pre][j - 1];
                     }
                 } else if(b[i - 1] == a[j - 1]) {
