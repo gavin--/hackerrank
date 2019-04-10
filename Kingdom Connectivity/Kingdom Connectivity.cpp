648440eaaf880b0bbe0f5393e86f2a76e9b97a74
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-04-10 13:14:02 -0400

    Update Kingdom Connectivity.cpp

--- Kingdom Connectivity/Kingdom Connectivity.cpp
+++ Kingdom Connectivity/Kingdom Connectivity.cpp
@@ -1,62 +1,60 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <functional>
 using namespace std;
 
-int *dp;
-vector<int> current;
 const int MOD = 1000000000;
-
-void dfs(int node, int n, vector<int> *graph, vector<bool>& visited, vector<bool>& cycle, set<int>& path) {
-    visited[node] = true;
-    current.emplace_back(node);
-    if(node == n - 1) {
-        for(auto i : current) {
-            path.emplace(i);
-            dp[i] = (dp[i] + 1) % MOD;
-        }
-    } else {
-        for(auto i : graph[node]) {
-            if(visited[i]) {
-                cycle[i] = true;
-            } else {
-                if(dp[i] > 0) {
-                    for(auto j : current) {
-                        dp[j] = (dp[j] + dp[i]) % MOD;
-                    }
-                } else if(dp[i] == 0) {
-                    dfs(i, n, graph, visited, cycle, path);
-                }
-            }
-        }
-    }
-    if(dp[node] == 0) {
-        dp[node] = -1;
-    }
-    current.pop_back();
-    visited[node] = false;
-}
-
-bool cycle(int n, vector<int> *graph) {
-    set<int> path;
-    vector<bool> visited(n), cycle(n);
-    dfs(0, n, graph, visited, cycle, path);
-    for(int i = 0; i < n; i++) {
-        if(cycle[i] && path.find(i) != path.end()) {
-            return true;
-        }
-    }
-    return false;
-}
 
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<int> graph[n];
-    dp = new int[n]();
+    vector<int> graph[n], current;
+    int dp[n];
+    fill(dp, dp + n, 0);
     for(int x, y; cin >> x >> y; graph[x - 1].emplace_back(y - 1)) {
     }
-    if(cycle(n, graph)) {
+    function<bool(int)> cycle = [&current, &graph, &dp](int n) {
+        set<int> path;
+        vector<bool> visited(n), cycle(n);
+        function<void(int)> dfs = [&n, &dfs, &graph, &visited, &cycle, &path, &dp, &current](int node) {
+            visited[node] = true;
+            current.emplace_back(node);
+            if(node == n - 1) {
+                for(auto i : current) {
+                    path.emplace(i);
+                    dp[i] = (dp[i] + 1) % MOD;
+                }
+            } else {
+                for(auto i : graph[node]) {
+                    if(visited[i]) {
+                        cycle[i] = true;
+                    } else {
+                        if(dp[i] > 0) {
+                            for(auto j : current) {
+                                dp[j] = (dp[j] + dp[i]) % MOD;
+                            }
+                        } else if(dp[i] == 0) {
+                            dfs(i);
+                        }
+                    }
+                }
+            }
+            if(dp[node] == 0) {
+                dp[node] = -1;
+            }
+            current.pop_back();
+            visited[node] = false;
+        };
+        dfs(0);
+        for(int i = 0; i < n; i++) {
+            if(cycle[i] && path.find(i) != path.end()) {
+                return true;
+            }
+        }
+        return false;
+    };
+    if(cycle(n)) {
         cout << "INFINITE PATHS";
     } else {
         cout << dp[0];
