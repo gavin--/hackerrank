6b4b20b4bf97d20c7700f0b36d93d67d532db1ac
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-04-16 18:19:12 -0400

    Update nCr table.cpp

--- nCr table/nCr table.cpp
+++ nCr table/nCr table.cpp
@@ -6,19 +6,18 @@ const int MOD = 1000000000;
 int main() {
     int T;
     cin >> T;
-    for(int n; cin >> n; ) {
+    for(int n; cin >> n; cout << '\n') {
         int dp[n + 1];
-        for(auto i = 1; i <= n; i++) {
+        for(int i = 1; i <= n; i++) {
             dp[0] = 1;
             dp[i] = 1;
-            for(auto j = i - 1; j > 0; j--) {
+            for(int j = i - 1; j > 0; j--) {
                 dp[j] = (dp[j - 1] + dp[j]) % MOD;
             }
         }
         for(auto i = 0; i <= n; i++) {
             cout << dp[i] << ' ';
         }
-        cout << endl;
     }
     return 0;
 }
