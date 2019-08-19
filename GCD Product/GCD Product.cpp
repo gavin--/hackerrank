fe12438816eba0f7c70a48b62064fc2778b6210d
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-08-19 15:51:45 -0400

    Update GCD Product.cpp

--- GCD Product/GCD Product.cpp
+++ GCD Product/GCD Product.cpp
@@ -17,40 +17,34 @@ long long pow(long long x, long long n) {
 }
 
 int main() {
-    long long N, M;
-    cin >> N >> M;
-    vector<bool> prime(N + 1, 1);
-    for (long long i = 2; i * i <= N; i++) {
+    long long size[2];
+    cin >> size[0] >> size[1];
+    vector<bool> prime(size[0] + 1, 1);
+    for (long long i = 2; i * i <= size[0]; i++) {
         if (prime[i]) {
-            for (long long j = i * i; j <= N; j += i) {
+            for (long long j = i * i; j <= size[0]; j += i) {
                 prime[j] = 0;
             }
         }
     }
     long long result = 1;
-    for (long long i = 2; i <= N; i++) {
+    for (long long i = 2; i <= size[0]; i++) {
         if (prime[i]) {
-            auto j = i;
-            while (j * i <= N) {
-                j *= i;
-            }
-            vector<long long> s, t;
-            for (long long sum = 0; j > 1; j /= i) {
-                s.emplace_back(N / j - sum);
-                sum += s.back();
-            }
-            j = i;
-            while (j * i <= M) {
-                j *= i;
-            }
-            for (long long sum = 0; j > 1; j /= i) {
-                t.emplace_back(M / j - sum);
-                sum += t.back();
+            vector<long long> count[2];
+            for (int k : {0, 1}) {
+                auto j = i;
+                while (j * i <= size[k]) {
+                    j *= i;
+                }
+                for (long long sum = 0; j > 1; j /= i) {
+                    count[k].emplace_back(size[k] / j - sum);
+                    sum += count[k].back();
+                }
             }
             long long exp = 0;
-            for (long long i = 1; i <= s.size(); i++) {
-                for (long long j = 1; j <= t.size(); j++) {
-                    exp += min(i, j) * s[s.size() - i] * t[t.size() - j];
+            for (long long i = 1; i <= count[0].size(); i++) {
+                for (long long j = 1; j <= count[1].size(); j++) {
+                    exp += min(i, j) * count[0][count[0].size() - i] * count[1][count[1].size() - j];
                 }
             }
             result = pow(i, exp) * result % MOD;
