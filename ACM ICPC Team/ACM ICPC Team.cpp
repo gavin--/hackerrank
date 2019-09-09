d631747de3b48e133f206553622a8b96b73ac42b
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-09-09 15:29:07 -0400

    Update ACM ICPC Team.cpp

--- ACM ICPC Team/ACM ICPC Team.cpp
+++ ACM ICPC Team/ACM ICPC Team.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int count(string& a, string& b) {
     int result = 0;
-    for(int i = 0; i != a.size(); i++) {
-        if(a[i] == '1' || b[i] == '1') {
+    for (int i = 0; i != a.size(); i++) {
+        if (a[i] == '1' || b[i] == '1') {
             result++;
         }
     }
@@ -15,13 +15,13 @@ int main() {
     int n, m, topic = 0, team;
     cin >> n >> m;
     string a[n];
-    for(int i = 0; cin >> a[i]; i++) {
-        for(int j = 0; j != i; j++) {
+    for (int i = 0; cin >> a[i]; i++) {
+        for (int j = 0; j != i; j++) {
             int current = count(a[i], a[j]);
-            if(current > topic) {
+            if (current > topic) {
                 topic = current;
                 team = 1;
-            } else if(current == topic) {
+            } else if (current == topic) {
                 team++;
             }
         }
