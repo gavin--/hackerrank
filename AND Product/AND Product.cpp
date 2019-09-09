18cdd6cd08bd4d6d55e2c7471a60f2ab5610ca4a
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-09-09 15:29:56 -0400

    Update AND Product.cpp

--- AND Product/AND Product.cpp
+++ AND Product/AND Product.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for(unsigned long long a, b; cin >> a >> b; ) {
+    for (unsigned long long a, b; cin >> a >> b; ) {
         auto c = ULLONG_MAX;
         b ^= a;
         do {
             b /= 2;
             c *= 2;
         } while (b != 0);
-        cout << (a & c) << endl;
+        cout << (a & c) << '\n';
     }
     return 0;
 }
