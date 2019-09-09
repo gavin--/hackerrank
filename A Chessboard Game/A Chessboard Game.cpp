7e1677b7a53aa99f9227d363992cfa318ba888b9
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-09-09 15:26:41 -0400

    Update A Chessboard Game.cpp

--- A Chessboard Game/A Chessboard Game.cpp
+++ A Chessboard Game/A Chessboard Game.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    int T;
-    cin >> T;
-    for(int x, y; cin >> x >> y; ) {
-        auto r1 = x % 4, r2 = y % 4;
-        if((r1 == 1 || r1 == 2) && (r2 == 1 || r2 == 2)) {
+    int t;
+    cin >> t;
+    for (int x, y; cin >> x >> y; ) {
+        int r1 = x % 4, r2 = y % 4;
+        if ((r1 == 1 || r1 == 2) && (r2 == 1 || r2 == 2)) {
             cout << "Second\n";
         } else {
             cout << "First\n";
