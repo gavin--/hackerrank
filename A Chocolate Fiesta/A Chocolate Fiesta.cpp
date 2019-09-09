5938528dc1407aad12fbfc8226bde64dc87f3a07
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-09-09 15:27:45 -0400

    Update A Chocolate Fiesta.cpp

--- A Chocolate Fiesta/A Chocolate Fiesta.cpp
+++ A Chocolate Fiesta/A Chocolate Fiesta.cpp
@@ -4,9 +4,9 @@ using namespace std;
 const long long MOD = 1000000007;
 
 long long pow(long long x, long long n) {
-    auto result = 1ll;
-    while(n != 0) {
-        if(n % 2 == 1) {
+    long long result = 1;
+    while (n != 0) {
+        if (n % 2 == 1) {
             result = result * x % MOD;
         }
         n /= 2;
@@ -16,10 +16,10 @@ long long pow(long long x, long long n) {
 }
 
 int main() {
-    int N;
-    cin >> N;
+    int n;
+    cin >> n;
     long long parity[2] = {};
-    for(int i; cin >> i; parity[i % 2]++) {
+    for (int i; cin >> i; parity[i % 2]++) {
     }
     cout << (pow(2, parity[0]) * pow(2, parity[1] - 1) + MOD - 1) % MOD;
     return 0;
