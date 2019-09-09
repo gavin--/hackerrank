c5f98573ebf8d783ad5f26cf660900557613c19d
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-09-09 15:26:05 -0400

    Update 2D Array - DS.cpp

--- 2D Array - DS/2D Array - DS.cpp
+++ 2D Array - DS/2D Array - DS.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main(){
     int A[6][6], result = -9 * 7;
-    for(int i = 0; i < 6; i++) {
-        for(int j = 0; j < 6; j++) {
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 6; j++) {
             cin >> A[i][j];
-            if(i >= 2 && j >= 2) {
+            if (i >= 2 && j >= 2) {
                 int sum = A[i - 2][j - 2] + A[i - 2][j - 1] + A[i - 2][j];
                 sum += A[i][j - 2] + A[i][j - 1] + A[i][j];
                 sum += A[i - 1][j - 1];
