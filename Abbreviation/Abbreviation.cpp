45d15b3cb6b94d389db0ecc116ba8694f396f7ac
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-09-13 15:45:44 -0400

    Update Abbreviation.cpp

--- Abbreviation/Abbreviation.cpp
+++ Abbreviation/Abbreviation.cpp
@@ -4,29 +4,29 @@ using namespace std;
 int main() {
     int q;
     cin >> q;
-    for(string a, b; cin >> a >> b; ) {
+    for (string a, b; cin >> a >> b; ) {
         bool dp[2][a.size() + 1], cur = 0, pre = 1;
         dp[0][0] = 1;
-        for(auto i = 1; i <= a.size(); i++) {
+        for (int i = 1; i <= a.size(); i++) {
             dp[0][i] = islower(a[i - 1]);
         }
-        for(int i = 1; i <= b.size(); i++) {
+        for (int i = 1; i <= b.size(); i++) {
             swap(cur, pre);
             dp[cur][i - 1] = 0;
-            for(int j = i; j <= a.size(); j++) {
-                if(islower(a[j - 1])) {
+            for (int j = i; j <= a.size(); j++) {
+                if (islower(a[j - 1])) {
                     dp[cur][j] = dp[cur][j - 1];
-                    if(a[j - 1] == tolower(b[i - 1])) {
+                    if (a[j - 1] == tolower(b[i - 1])) {
                         dp[cur][j] = dp[cur][j] || dp[pre][j - 1];
                     }
-                } else if(b[i - 1] == a[j - 1]) {
+                } else if (b[i - 1] == a[j - 1]) {
                     dp[cur][j] = dp[pre][j - 1];
                 } else {
                     dp[cur][j] = 0;
                 }
             }
         }
-        if(dp[cur][a.size()]) {
+        if (dp[cur][a.size()]) {
             cout << "YES\n";
         } else {
             cout << "NO\n";
