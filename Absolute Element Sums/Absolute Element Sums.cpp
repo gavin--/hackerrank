32da70e110ff9a5f5d7f5700521b8a52e4182352
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-09-13 15:47:21 -0400

    Update Absolute Element Sums.cpp

--- Absolute Element Sums/Absolute Element Sums.cpp
+++ Absolute Element Sums/Absolute Element Sums.cpp
@@ -6,28 +6,28 @@ int main() {
     int n, q;
     cin >> n;
     int a[n];
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
     sort(a, a + n);
     long long prefix[n + 1], sum = 0;
     prefix[0] = 0;
-    for(int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) {
         prefix[i] = prefix[i - 1] + a[i - 1];
     }
     cin >> q;
-    for(long long x; cin >> x; ) {
+    for (long long x; cin >> x; ) {
         sum += x;
         int begin = 0, end = n - 1;
-        while(begin < end) {
+        while (begin < end) {
             auto mid = (begin + end) / 2 + 1;
-            if(a[mid] + sum < 0) {
+            if (a[mid] + sum < 0) {
                 begin = mid;
             } else {
                 end = mid - 1;
             }
         }
-        if(a[begin] + sum >= 0) {
+        if (a[begin] + sum >= 0) {
             begin = -1;
         }
         long long result = prefix[n] - prefix[begin + 1] + sum * (n - begin - 1) - (prefix[begin + 1] + sum * (begin + 1));
