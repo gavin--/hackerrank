bd60c2622a19ccd5f850a398437a7026d5db3de1
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-09-18 13:31:11 -0400

    Update Easy GCD.cpp

--- Easy GCD/Easy GCD.cpp
+++ Easy GCD/Easy GCD.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int gcd(int a, int b) {
-    while(b != 0) {
+    while (b != 0) {
         auto t = b;
         b = a % b;
         a = t;
@@ -14,20 +14,20 @@ int gcd(int a, int b) {
 int main() {
     int n, k, g = 0, result = 0;
     cin >> n >> k;
-    for(int a; cin >> a; ) {
+    for (int a; cin >> a; ) {
         g = gcd(g, a);
     }
-    for(auto i = 2; i * i <= g; i++) {
-        if(g % i == 0) {
-            if(i <= k) {
+    for (int i = 2; i * i <= g; i++) {
+        if (g % i == 0) {
+            if (i <= k) {
                 result = max(result, k / i * i);
             }
-            if(g / i <= k) {
+            if (g / i <= k) {
                 result = max(result, k / (g / i) * i);
             }
         }
     }
-    if(g <= k) {
+    if (g <= k) {
         result = max(result, k / g * g);
     }
     cout << result;
