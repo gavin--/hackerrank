1d7aa60f9c02e2130da1ac7482bd3c9ae1de8a83
gavin-- <gavinzhubbb4mp5@gmail.com>
2019-12-11 20:00:29 -0500

    Update Minimum Swaps 2.cpp

--- Minimum Swaps 2/Minimum Swaps 2.cpp
+++ Minimum Swaps 2/Minimum Swaps 2.cpp
@@ -5,16 +5,13 @@ int main() {
     int n, result = 0;
     cin >> n;
     int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    for (int i = 0; cin >> arr[i]; i++) {
     }
     for (int i = 0; i < n; i++) {
-        int current = 0;
         while (arr[i] != i + 1) {
             swap(arr[i], arr[arr[i] - 1]);
-            current++;
+            result++;
         }
-        result += current;
     }
     cout << result;
     return 0;
